bd485e90cc8b4f1b7868b8c58283b72266e5f69e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2022-11-26 17:50:34 +0000

    [LeetCode Sync] Runtime - 1 ms (99.44%), Memory - 52.3 MB (99.99%)

--- Leetcode problems/0053-maximum-subarray/solution.java
+++ Leetcode problems/0053-maximum-subarray/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSubsum= nums[0];
-        int Maxsubsum = nums[0];
-        
-        for (int i =1; i<nums.length;i++)
+         int currentsubarray=nums[0];
+         int maxsubarray=nums[0];
+        for(int i=1;i<nums.length;i++)
         {
-            int num = nums[i];
-            currentSubsum = Math.max(num, currentSubsum+num);
-            Maxsubsum = Math.max(Maxsubsum, currentSubsum);
+            int num=nums[i];
+            currentsubarray=Math.max(num,currentsubarray+num);
+            maxsubarray=Math.max(currentsubarray,maxsubarray);
         }
-        return Maxsubsum;
+        return maxsubarray;
     }
 }
