236eed7bd9941af4fc3dcc0206ea4bf630a80def
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2022-11-28 18:38:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (37.54%)

--- Leetcode problems/0033-search-in-rotated-sorted-array/solution.java
+++ Leetcode problems/0033-search-in-rotated-sorted-array/solution.java
@@ -1,26 +1,59 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        int mid;
-        
-        while(low<=high) {
-            mid = (low+high)/2;
-            if (target == nums[mid]) 
-                return mid;
-            else if (nums[low]<=nums[mid]) {
-                if(target>= nums[low] && target<nums[mid])
-                    high = mid-1;
+    public int findrotateindex(int []nums,int left,int right)
+    {
+        if(nums[left]<nums[right])
+            return 0;
+        while(left<=right)
+        {
+            int pivot=(left+right)/2;
+            if(nums[pivot]>nums[pivot+1])
+                return pivot+1;
+            else
+            {
+                if(nums[pivot]<nums[left])
+                    right=pivot-1;
                 else
-                    low = mid+1;
-            } else {
-                if(target>nums[mid] && target<=nums[high])
-                    low = mid+1;
-                else
-                    high = mid-1;
+                    left=pivot+1;
             }
         }
+        return 0;
+    }
+    public int search(int[] nums,int left,int right,int target)
+    {
+        while(right>=left)
+        {
+            int mid=(right+left)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+            
+            
+        }
         return -1;
-    }   
-}
+    }
+    public int search(int[] nums, int target) {
+        
+        int n=nums.length;
+        if(n==1){
+            if(nums[0]==target)
+                return 0;
+            else
+                return -1;
+        }
+        int rotateindex=findrotateindex(nums,0,n-1);
+        if(nums[rotateindex]==target)
+            return rotateindex;
+        if(rotateindex==0)
+            return search(nums,0,n-1,target);
+        if(target<nums[0])
+            return search(nums,rotateindex,n-1,target);
+        return search(nums,0,rotateindex,target);
+                
+            
+        }
+        
+    }
 
