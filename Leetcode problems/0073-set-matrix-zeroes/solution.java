62519633e35a3f8efcf7565989a3e8c3875a5e57
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2022-11-29 21:11:05 +0000

    [LeetCode Sync] Runtime - 5 ms (6.66%), Memory - 54.2 MB (7.13%)

--- Leetcode problems/0073-set-matrix-zeroes/solution.java
+++ Leetcode problems/0073-set-matrix-zeroes/solution.java
@@ -1,40 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean iscol=false;
-        int r=matrix.length;
+        int R=matrix.length;
         int c=matrix[0].length;
-        for(int i=0;i<r;i++)
-        {
-            if(matrix[i][0]==0)
-                iscol=true;
-            for(int j=1;j<c;j++)
-            {
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
-                }
-                    
-                
-            }
-        }
-        for(int i=1;i<r;i++){
-            for(int j=1;j<c;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        if(matrix[0][0]==0)
+        Set<Integer> rows=new HashSet<Integer>();
+        Set<Integer> cols=new HashSet<Integer>();
+        for(int i=0;i<R;i++)
         {
             for(int j=0;j<c;j++)
             {
-                matrix[0][j]=0;
+                if(matrix[i][j]==0)
+                   {
+                       rows.add(i);
+                        cols.add(j);
+                   }
             }
         }
-        if(iscol){
-            for(int i=0;i<r;i++)
+        for(int i=0;i<R;i++)
+        {
+            for(int j=0;j<c;j++)
             {
-                matrix[i][0]=0;
+                if(rows.contains(i)||cols.contains(j))
+                    matrix[i][j]=0;
             }
         }
         
