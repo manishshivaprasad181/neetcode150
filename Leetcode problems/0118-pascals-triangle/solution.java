ec6c55c19f9cb192aa12b66b25e33ce2a2d4889f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-01-02 20:07:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (99.99%)

--- Leetcode problems/0118-pascals-triangle/solution.java
+++ Leetcode problems/0118-pascals-triangle/solution.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle=new ArrayList<>();
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
+        List<List<Integer>> Triangle=new ArrayList<List<Integer>>();
+        Triangle.add(new ArrayList<>());
+        Triangle.get(0).add(1);
         for(int rownum=1;rownum<numRows;rownum++)
         {
-            List<Integer> currrow=new ArrayList<>();
-            List<Integer> prevrow=triangle.get(rownum-1);
-            currrow.add(1);
-            for(int col=1;col<prevrow.size();col++){
-                currrow.add(prevrow.get(col-1)+prevrow.get(col));
-            }
-            currrow.add(1);
-            triangle.add(currrow);
-            
+          List<Integer> row= new ArrayList<>();
+          List<Integer> prevrow= Triangle.get(rownum-1);
+          row.add(1);
+          for(int j=1;j<rownum;j++)
+          {
+            row.add(prevrow.get(j-1)+prevrow.get(j));
+          }
+          row.add(1);
+          Triangle.add(row);
+
+          
+
         }
-        return triangle;
-        
+        return Triangle;
     }
 }
