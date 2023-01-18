6cdeff15b8d02c9b303e7cb35d5b08fd08be74a1
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-01-18 23:09:11 +0000

    [LeetCode Sync] Runtime - 278 ms (9.66%), Memory - 42.3 MB (20.78%)

---
+++ Leetcode problems/0079-word-search/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    private char[][] board;
+    private int rows;
+    private int cols;
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.rows = board.length;
+        this.cols = board[0].length;
+        for(int i =0;i<rows;i++)
+            for(int j =0;j<cols;j++)    
+                if(backtrack(i, j, word, 0))
+                    return true;
+        return false;
+     }
+     private boolean backtrack(int row, int col, String word, int index) {
+         if(index>=word.length())
+            return true;
+        if(row<0||row==this.rows|| col<0||col == this.cols|| this.board[row][col]!= word.charAt(index))
+           return false;
+        int[] rowoffset = {0,1,0,-1};
+        int[] coloffset = {1,0,-1,0};
+        boolean ret = false;
+        this.board[row][col] = '#';
+        for(int i =0;i<4;i++) {
+            ret = backtrack(row+rowoffset[i], col+coloffset[i], word, index+1);
+            if(ret)
+                break;
+        }
+        this.board[row][col] = word.charAt(index);
+        return ret;
+     }
+
+}
