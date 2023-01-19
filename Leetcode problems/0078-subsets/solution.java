b14533849be3d69ff85b48bf82465056115d9423
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-01-19 18:17:17 +0000

    [LeetCode Sync] Runtime - 1 ms (60.95%), Memory - 42.7 MB (45.91%)

---
+++ Leetcode problems/0078-subsets/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+       List<List<Integer>> output = new ArrayList<>();
+       output.add(new ArrayList<Integer>());
+
+       for(int num: nums) {
+           List<List<Integer>> newsubsets = new ArrayList<>();
+           for(List<Integer> curr: output) {
+               newsubsets.add(new ArrayList<Integer>(curr){{add(num);}});
+           }
+           for(List<Integer> curr: newsubsets)
+                output.add(curr);
+       }
+       return output;
+           
+    } 
+}
+
