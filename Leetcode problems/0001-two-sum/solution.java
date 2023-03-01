3666a1c73db40c1d0272f18717603e393edf3738
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-03-01 03:55:08 +0000

    [LeetCode Sync] Runtime - 1 ms (97.78%), Memory - 42.4 MB (99.99%)

--- Leetcode problems/0001-two-sum/solution.java
+++ Leetcode problems/0001-two-sum/solution.java
@@ -3,14 +3,11 @@ class Solution {
         HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-            int complement=target-nums[i];
-            if(hm.containsKey(complement))
-            {
-                return new int[]{i,hm.get(complement)};
-            }
-            else{
+            int diff=target-nums[i];
+            if(hm.containsKey(diff))
+                return new int[]{i,hm.get(diff)};
+            else
                 hm.put(nums[i],i);
-            }
         }
         return null;
         
