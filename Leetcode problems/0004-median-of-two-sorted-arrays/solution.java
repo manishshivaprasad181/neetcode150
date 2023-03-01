75c968739146e3033a8a31c1c89093c8ea7194ce
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-03-01 19:56:05 +0000

    [LeetCode Sync] Runtime - 2 ms (64.47%), Memory - 43.3 MB (99.99%)

--- Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
+++ Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
@@ -1,37 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int s1 = nums1.length;
-        int s2 = nums2.length;
-        double[] temp = new double[s1+s2];
-        int i =0,j=0,k=0;
-        while(i<s1 && j<s2) {
-            if(nums1[i]<nums2[j]) {
-                temp[k] = nums1[i];
-                i++;
-                k++;
-            } else {
-                temp[k] = nums2[j];
-                j++;
-                k++;
+        int i=0,j=0,k=0;
+        int[] result=new int[nums1.length+nums2.length];
+        while(i<nums1.length && j<nums2.length)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                result[k++]=nums1[i++];
+            }
+            else{
+                result[k++]=nums2[j++];
             }
         }
-        if(i>=s1) {
-            for(int t=j;t<s2;t++) {
-                temp[k] = nums2[t];
-                k++;
+            while(i<nums1.length)
+            result[k++]=nums1[i++];
+            while(j<nums2.length)
+            result[k++]=nums2[j++];
+            if(result.length%2==0)
+            {   int middleIndex1 = result.length/ 2 - 1;
+                int middleIndex2 = result.length / 2;
+                return (result[middleIndex1]+result[middleIndex2])/2.0;
             }
-        }
-        if(j>=s2) {
-            for(int t=i;t<s1;t++) {
-                temp[k] = nums1[t];
-                k++;
-             }
-        }
-        double ans =0.0;
-        if(temp.length%2!=0) 
-            return temp[temp.length/2];
-        else
-            return (temp[temp.length/2] + temp[temp.length/2-1])/2;
+            else{
+                return result[(int)(result.length/2)];
+            }
 
+        }
+
+        
     }
-}
+
