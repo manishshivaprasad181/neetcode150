72260bbb8a5b855aa75614bbdb7ab90afef132a1
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-03-18 22:46:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (14.74%)

--- Leetcode problems/0088-merge-sorted-array/solution.java
+++ Leetcode problems/0088-merge-sorted-array/solution.java
@@ -5,13 +5,11 @@ class Solution {
         for(int p=m+n-1;p>=0;p--)
         {
             if(p2<0)
-                break;
-            if(p1>=0 && nums1[p1]>nums2[p2])
-                nums1[p]=nums1[p1--];
+            break;
+            if(p1>=0&&nums1[p1]>nums2[p2])
+            nums1[p]=nums1[p1--];
             else
-                nums1[p]=nums2[p2--];
-            
+            nums1[p]=nums2[p2--];
         }
-        
     }
 }
