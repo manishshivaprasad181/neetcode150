d00caed19d213a01ec89e85d82ecb3a1312075e8
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-04-23 22:26:37 +0000

    [LeetCode Sync] Runtime - 191 ms (17.30%), Memory - 40 MB (100.00%)

--- Leetcode problems/0473-matchsticks-to-square/solution.java
+++ Leetcode problems/0473-matchsticks-to-square/solution.java
@@ -1,23 +1,22 @@
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.*;
 class Solution {
     public List<Integer> nums;
     public int[] sums;
-    public int possibleSquareSide;
-
-    public Solution() {
-        this.sums = new int[4];
+    public int possiblesquareside;
+    public Solution(){
+        this.sums=new int[4];
     }
-    
-    public boolean dfs(int index) {
-        if(index == this.nums.size()) {
-            return sums[0]==sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+    public boolean dfs(int index){
+        if(index==this.nums.size()){
+            return sums[0]==sums[1] && sums[1]==sums[2] && sums[2]==sums[3];
         }
-
-        int element = this.nums.get(index);
-
-        for(int i =0;i< 4;i++) {
-            if(this.sums[i]+element <= this.possibleSquareSide) {
-                this.sums[i] += element;
-                if(this.dfs(index+1)) {
+        int element=this.nums.get(index);
+        for(int i=0;i<4;i++){
+            if(this.sums[i]+element<=this.possiblesquareside){
+                this.sums[i]+=element;
+                if(this.dfs(index+1)){
                     return true;
                 }
                 this.sums[i]-=element;
@@ -25,21 +24,21 @@ class Solution {
         }
         return false;
     }
-    public boolean makesquare(int[] nums) {
-        if(nums == null || nums.length==0)
-            return false;
-        
-        int L = nums.length;
-        int perimeter = 0;
-        for(int i =0;i<L;i++) {
-            perimeter+= nums[i];
+    public boolean makesquare(int[] matchsticks) {
+        if(matchsticks==null||matchsticks.length==0)
+        return false;
+        int l=matchsticks.length;
+        int perimeter=0;
+        for(int i=0;i<l;i++)
+        {
+            perimeter+=matchsticks[i];
         }
-        this.possibleSquareSide = perimeter/4;
-        if(this.possibleSquareSide*4!=perimeter) {
+        this.possiblesquareside=perimeter/4;
+        if(this.possiblesquareside*4!=perimeter){
             return false;
         }
-        this.nums = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        this.nums=Arrays.stream(matchsticks).boxed().collect(Collectors.toList());
         Collections.sort(this.nums,Collections.reverseOrder());
-        return this.dfs(0);    
+        return this.dfs(0);
     }
 }
