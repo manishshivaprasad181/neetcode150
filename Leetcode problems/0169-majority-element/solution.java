9e9d039fe99a5541be350978443c9615b84dbc07
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-04-29 08:42:03 +0000

    [LeetCode Sync] Runtime - 15 ms (20.52%), Memory - 47.8 MB (99.74%)

--- Leetcode problems/0169-majority-element/solution.java
+++ Leetcode problems/0169-majority-element/solution.java
@@ -1,27 +1,27 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> ht=new HashMap<Integer,Integer>();
-        int n=nums.length;
-        for(int i=0;i<nums.length;i++)
-        {
+        HashMap<Integer, Integer> ht = new HashMap<>();
+        int n = nums.length;
+        for(int i =0;i<n;i++) {
             if(!ht.containsKey(nums[i]))
-            {
                 ht.put(nums[i],1);
-            }
-            else
-            ht.put(nums[i],ht.get(nums[i])+1);
+            else   
+                ht.put(nums[i], ht.get(nums[i])+1);
         }
-        int maxvalue=Collections.max(ht.values());
-        for (Map.Entry<Integer, Integer> entry : ht.entrySet()) {
- 
-            if (entry.getValue() == maxvalue) {
+        int max_value = Collections.max(ht.values());
+        for(Map.Entry<Integer, Integer> entry : ht.entrySet()) {
+            if(entry.getValue()== max_value)
                 return entry.getKey();
-
-            }
-
         }
         return 0;
+
+        
+
+        
+        
+
+
     }
    
 }
