e5b392f9e1978592447e67d0dae25218a222c753
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-09 11:11:07 +0000

    [LeetCode Sync] Runtime - 4 ms (10.80%), Memory - 42.9 MB (100.00%)

---
+++ Leetcode problems/0572-subtree-of-another-tree/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null)
+        return false;
+        if(isIdentical(root,subRoot))
+        return true;
+        return isSubtree(root.right,subRoot)||isSubtree(root.left,subRoot);
+        
+    }
+    public boolean isIdentical(TreeNode node1,TreeNode node2)
+    {
+        if(node1==null || node2==null)
+        return node1==null && node2==null;
+        return node1.val==node2.val && isIdentical(node1.left,node2.left) && isIdentical(node1.right,node2.right);
+    }
+}
