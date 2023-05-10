082b7d6e44f3f13e04ddb4cb7afb74fa6d879d9e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-10 21:12:53 +0000

    [LeetCode Sync] Runtime - 15 ms (59.60%), Memory - 43.5 MB (99.98%)

--- Leetcode problems/0451-sort-characters-by-frequency/solution.java
+++ Leetcode problems/0451-sort-characters-by-frequency/solution.java
@@ -1,27 +1,19 @@
 class Solution {
     public String frequencySort(String s) {
-        if(s==null||s.isEmpty())
-            return s;
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-
-        List<String> charString = new ArrayList<>();
-        StringBuilder currentString = new StringBuilder();
-        currentString.append(chars[0]);
-        for(int i =1;i<chars.length;i++) {
-            if(chars[i]!=chars[i-1]) {
-                charString.add(currentString.toString());
-                currentString = new StringBuilder();
-            }
-            currentString.append(chars[i]);
-        }
-        charString.add(currentString.toString());
-
-        Collections.sort(charString, (a,b)->b.length()-a.length());
-
+        Map<Character, Integer> counts = new HashMap<>();
+        for(char c: s.toCharArray()) 
+            counts.put(c, counts.getOrDefault(c, 0)+1);
+        
+        List<Character> characters = new ArrayList<>(counts.keySet());
+        Collections.sort(characters, (a,b)->counts.get(b)-counts.get(a));
         StringBuilder sb = new StringBuilder();
-        for(String str:charString)
-            sb.append(str);
-        return sb.toString();
+        for(char c: characters) {
+            int copies = counts.get(c);
+            for(int i=0;i<copies;i++) {
+                sb.append(c);
+            }
+        }
+    return sb.toString();
     }
+
 }
