b08fb649cc192ed5de1ded0bacc45f7a91cdcee0
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-10 13:54:07 +0000

    [LeetCode Sync] Runtime - 13 ms (12.00%), Memory - 43.5 MB (92.00%)

---
+++ Leetcode problems/1075-index-pairs-of-a-string/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] indexPairs(String text, String[] words) {
+        List<int[]> result=new ArrayList();
+        Set<String> wordsset=Arrays.stream(words).collect(Collectors.toSet());
+        for(int i=0;i<text.length();i++)
+        {
+            for(int j=i;j<text.length();j++)
+            {
+                if(wordsset.contains(text.substring(i,j+1))){
+                    result.add(new int[] {i,j});
+                }
+            }
+        }
+        int ans[][]=new int[result.size()][];
+        ans=result.toArray(ans);
+        return ans;
+    }
+}
