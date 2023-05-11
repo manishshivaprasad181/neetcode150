d29079cb39556ac6aa8d504d98ef5d6b42f7330e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-11 08:57:00 +0000

    [LeetCode Sync] Runtime - 25 ms (73.91%), Memory - 42.9 MB (20.09%)

--- Leetcode problems/0133-clone-graph/solution.java
+++ Leetcode problems/0133-clone-graph/solution.java
@@ -19,19 +19,19 @@ class Node {
 */
 
 class Solution {
-    private HashMap<Node, Node> visited = new HashMap<>();
+    private Map<Node, Node> visited= new HashMap<>();
     public Node cloneGraph(Node node) {
-        if(node == null)
+        if(node ==null)
             return node;
-        
-        if(visited.containsKey(node)) {
+        if(visited.containsKey(node))
             return visited.get(node);
-        }
-        Node cloneNode = new Node(node.val, new ArrayList());
-        visited.put(node,cloneNode);
+        Node clonenode = new Node(node.val, new ArrayList());
+        visited.put(node, clonenode);
+
         for(Node neighbor:node.neighbors) {
-            cloneNode.neighbors.add(cloneGraph(neighbor));
-        } 
-        return cloneNode;
+            clonenode.neighbors.add(cloneGraph(neighbor));
+        }
+    return clonenode;       
+        
     }
 }
