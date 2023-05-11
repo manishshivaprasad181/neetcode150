60cc0baa20f0f3d697c78d462f38a4a32e1f7f0c
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-11 17:36:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (91.82%)

--- Leetcode problems/0054-spiral-matrix/solution.java
+++ Leetcode problems/0054-spiral-matrix/solution.java
@@ -3,30 +3,29 @@ class Solution {
       List<Integer> result = new ArrayList<>();
       int rows = matrix.length;
       int cols = matrix[0].length;
-      int left =0;
       int up =0;
-      int right = cols-1;
       int down = rows-1;
-
+      int left=0;
+      int right=cols-1;
       while(result.size()<rows*cols) {
-          for(int col=left;col<=right;col++)
-            result.add(matrix[up][col]);
-          for(int row=up+1;row<=down;row++) 
-            result.add(matrix[row][right]);
+        for(int col=left;col<=right;col++)
+          result.add(matrix[up][col]);
+        for(int row=up+1;row<=down;row++)
+          result.add(matrix[row][right]);
         if(up!=down) {
-            for(int col=right-1;col>=left;col--) 
-                result.add(matrix[down][col]);
+          for(int col=right-1;col>=left;col--)
+            result.add(matrix[down][col]);
         }
         if(left!=right) {
-            for(int row=down-1;row>up;row--)
-                result.add(matrix[row][left]);
+          for(int row=down-1;row>up;row--)
+            result.add(matrix[row][left]);
+
         }
-        left++;
-        right--;
         up++;
         down--;
+        left++;
+        right--;
       }
       return result;
-
     }
 }
