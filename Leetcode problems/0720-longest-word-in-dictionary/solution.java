4c266967c8664ac3dc523374b7164f4c9b49d752
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-12 22:21:38 +0000

    [LeetCode Sync] Runtime - 11 ms (64.94%), Memory - 42.8 MB (100.00%)

--- Leetcode problems/0720-longest-word-in-dictionary/solution.java
+++ Leetcode problems/0720-longest-word-in-dictionary/solution.java
@@ -3,18 +3,19 @@ class Solution {
         String ans="";
         Set<String> wordset = new HashSet<>();
         for(String word:words) wordset.add(word);
-        for(String word:words) {
-            if(word.length()>ans.length() || word.length()==ans.length() && word.compareTo(ans)<0) {
+
+        for(String word:wordset)  {
+            if(word.length()>ans.length() || word.length()==ans.length() &&word.compareTo(ans)<0) {
                 boolean good = true;
-                for(int k =1;k<word.length();k++) {
-                    if(!wordset.contains(word.substring(0,k))) {
+                for(int i =1;i<word.length();i++) {
+                    if(!wordset.contains(word.substring(0,i))) {
                         good = false;
                         break;
-
                     }
                 }
                 if(good) ans = word;
             }
+
         }
         return ans;
     }
