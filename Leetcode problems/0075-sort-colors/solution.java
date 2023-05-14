b70644700439788e285336b90861a1f5567eb433
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-14 17:11:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (99.91%)

--- Leetcode problems/0075-sort-colors/solution.java
+++ Leetcode problems/0075-sort-colors/solution.java
@@ -3,19 +3,18 @@ class Solution {
         int p0=0;
         int p2=nums.length-1;
         int curr=0;
-        int tmp =0;
+        int tmp=0;
         while(curr<=p2) {
             if(nums[curr]==0) {
-                tmp = nums[curr];
-                nums[curr++]= nums[p0];
-                nums[p0++] = tmp;
+                tmp =nums[curr];
+                nums[curr++] = nums[p0];
+                nums[p0++]=tmp;
             } else if(nums[curr]==2) {
-                tmp = nums[curr];
-                nums[curr] = nums[p2];
+                tmp=nums[curr];
+                nums[curr] =nums[p2];
                 nums[p2--] = tmp;
-            } else  
+            } else
                 curr++;
         }
-
     }
 }
