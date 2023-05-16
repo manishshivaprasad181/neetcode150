acaf6c73ccf993b4734b6c521d3f8aa59d8e3d44
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-16 15:58:08 +0000

    [LeetCode Sync] Runtime - 9 ms (56.83%), Memory - 47.7 MB (5.62%)

--- Leetcode problems/0056-merge-intervals/solution.java
+++ Leetcode problems/0056-merge-intervals/solution.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        List<int[]> mergedIntervals=new ArrayList<>();
-        int [] currentinterval=intervals[0];
-        for(int i=1;i<intervals.length;i++)
-        {
-            if (intervals[i][0] <= currentinterval[1])
-           currentinterval[1] = Math.max(currentinterval[1], intervals[i][1]);
-         else {
-            mergedIntervals.add(currentinterval);
-            currentinterval = intervals[i];
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        for(int i=1;i<intervals.length;i++) {
+            if(intervals[i][0]<=currentInterval[1]) {
+                currentInterval[1] =Math.max(currentInterval[1],intervals[i][1]);
+
+            } else {
+                mergedIntervals.add(currentInterval);
+                currentInterval=intervals[i];
+            }
         }
-    }
-    
-    mergedIntervals.add(currentinterval); // Step 7
-    
-    return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+        mergedIntervals.add(currentInterval);
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
         }
-        
-        
     }
 
