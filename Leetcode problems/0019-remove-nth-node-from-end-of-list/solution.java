24604d903726802be959ef4853e076074a341004
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-19 21:13:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.6 MB (99.95%)

--- Leetcode problems/0019-remove-nth-node-from-end-of-list/solution.java
+++ Leetcode problems/0019-remove-nth-node-from-end-of-list/solution.java
@@ -14,15 +14,21 @@ class Solution {
         dummy.next = head;
         ListNode first = dummy;
         ListNode second = dummy;
-        for(int i =1;i<=n+1;i++) {
-            first = first.next;
+
+        for(int i=1;i<=n+1;i++) {
+            first= first.next;
         }
         while(first!=null) {
-            first = first.next;
+            first=first.next;
             second = second.next;
         }
         second.next = second.next.next;
         return dummy.next;
-        
     }
 }
+
+
+
+
+
+
