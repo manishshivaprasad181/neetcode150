4f402fa0c56ccfbc3e7b44697cdf103133fa3766
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-19 21:43:22 +0000

    [LeetCode Sync] Runtime - 1 ms (93.04%), Memory - 44.8 MB (97.24%)

--- Leetcode problems/0203-remove-linked-list-elements/solution.java
+++ Leetcode problems/0203-remove-linked-list-elements/solution.java
@@ -10,17 +10,16 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode sentinel=new ListNode(0);
-        sentinel.next=head;
-        ListNode curr=head,prev=sentinel;
-        while(curr!=null)
-        {
-            if(curr.val==val)prev.next=curr.next;
-            else
+      ListNode dummy = new ListNode(0);
+      dummy.next = head;
+      ListNode curr = head;
+      ListNode prev = dummy;
+      while(curr!=null) {
+          if(curr.val==val) prev.next= curr.next;
+          else 
             prev=curr;
-            curr=curr.next;
-        }
-    
-        return sentinel.next;
+        curr=curr.next;
+      }
+      return dummy.next;
     }
 }
