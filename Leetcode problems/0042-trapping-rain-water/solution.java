f18e7f423157b452a414075e7306a753ef5b091f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-20 19:10:18 +0000

    [LeetCode Sync] Runtime - 1 ms (71.23%), Memory - 44.8 MB (99.21%)

--- Leetcode problems/0042-trapping-rain-water/solution.java
+++ Leetcode problems/0042-trapping-rain-water/solution.java
@@ -1,21 +1,25 @@
 class Solution {
     public int trap(int[] height) {
-        int ans =0;
-        int size = height.length;
-        if(size==0) 
-            return 0;
-        int[] left_max = new int[size];
-        int[] right_max = new int[size];
-        left_max[0] = height[0];
-        for(int i=1;i<size;i++) {
-            left_max[i] = Math.max(height[i], left_max[i-1]);
-        }
-        right_max[size-1]= height[size-1];
-        for(int i=size-2;i>=0;i--) {
-            right_max[i] = Math.max(height[i], right_max[i+1]);
-        }
-        for(int i =1;i<size-1;i++) {
-            ans+= Math.min(left_max[i], right_max[i])-height[i];
+        int ans=0;
+        int left=0;
+        int right = height.length-1;
+        int left_max =0;
+        int right_max=0;
+        while(left<right) {
+            if(height[left]<height[right]) {
+                if(height[left]>=left_max)
+                    left_max=height[left];
+                else
+                    ans+= left_max-height[left];
+                    left++;
+            } else {
+                if(height[right]>=right_max) 
+                    right_max=height[right];
+                else    
+                    ans+= right_max-height[right];
+                right--;
+            }
+
         }
         return ans;
     }
