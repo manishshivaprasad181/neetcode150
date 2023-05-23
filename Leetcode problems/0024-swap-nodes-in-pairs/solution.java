0e73c1e7caf1b15375df185f64c5a9b9cc2767ee
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-23 19:19:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (99.61%)

--- Leetcode problems/0024-swap-nodes-in-pairs/solution.java
+++ Leetcode problems/0024-swap-nodes-in-pairs/solution.java
@@ -11,11 +11,14 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
         if(head==null||head.next==null)
-        return head;
-        ListNode firstnode=head;
-        ListNode secondnode=head.next;
-        firstnode.next=swapPairs(secondnode.next);
-        secondnode.next=firstnode;
-        return secondnode;
+            return head;
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        firstNode.next = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        return secondNode;
+        
     }
 }
