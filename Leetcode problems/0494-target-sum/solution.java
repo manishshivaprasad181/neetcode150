8f4b60a4bf8b261c9279726ba729906ad30f9b3b
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-25 13:03:09 +0000

    [LeetCode Sync] Runtime - 576 ms (33.85%), Memory - 40.5 MB (99.20%)

--- Leetcode problems/0494-target-sum/solution.java
+++ Leetcode problems/0494-target-sum/solution.java
@@ -1,16 +1,20 @@
 class Solution {
-    int count =0;
+    int count=0;
     public int findTargetSumWays(int[] nums, int target) {
-        calculate(nums, 0,0,target);    
+        backtrack(nums,0,0,target);
         return count;
     }
-    private void calculate(int[] nums, int i, int sum,int target) {
-        if(i==nums.length) {
+    public void backtrack(int[] nums,int i,int sum,int target)
+    {
+        if(i==nums.length)
+        {
             if(sum==target)
-                count++;
-        } else {
-            calculate(nums, i+1, sum+nums[i], target);
-            calculate(nums, i+1, sum-nums[i], target);
+            count++;
+        }
+        else
+        {
+            backtrack(nums,i+1,sum+nums[i],target);
+            backtrack(nums,i+1,sum-nums[i],target);
         }
     }
 }
