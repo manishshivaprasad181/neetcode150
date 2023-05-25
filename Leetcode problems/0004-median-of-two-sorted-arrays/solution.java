9dd34f12b7c66b1e3e863bd041b780314aa21738
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-25 02:02:43 +0000

    [LeetCode Sync] Runtime - 2 ms (64.47%), Memory - 44.8 MB (99.93%)

--- Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
+++ Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
@@ -1,32 +1,42 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int s1 = nums1.length;
+        int s2 = nums2.length;
+        double[] temp = new double[s1+s2];
         int i=0,j=0,k=0;
-        int[] result=new int[nums1.length+nums2.length];
-        while(i<nums1.length && j<nums2.length)
-        {
-            if(nums1[i]<=nums2[j])
-            {
-                result[k++]=nums1[i++];
-            }
-            else{
-                result[k++]=nums2[j++];
+        while(i<s1&&j<s2) {
+            if(nums1[i]<nums2[j]) {
+                temp[k] = nums1[i];
+                k++;
+                i++;
+            } else {
+                temp[k] = nums2[j];
+                k++;
+                j++;
             }
         }
-            while(i<nums1.length)
-            result[k++]=nums1[i++];
-            while(j<nums2.length)
-            result[k++]=nums2[j++];
-            if(result.length%2==0)
-            {   int middleIndex1 = result.length/ 2 - 1;
-                int middleIndex2 = result.length / 2;
-                return (result[middleIndex1]+result[middleIndex2])/2.0;
-            }
-            else{
-                return result[(int)(result.length/2)];
-            }
+        if(i>=s1) {
+            for(int m =j;m<s2;m++) {
+                temp[k] = nums2[m];
+                k++;
 
-        }
-
+            }
         
-    }
+        }
+        if(j>=s2) {
+            for(int n =i;n<s1;n++) {
+                temp[k] = nums1[n];
+                k++;
 
+            }
+        
+        }
+        double ans = 0.0;
+        if(temp.length%2!=0) {
+            ans = temp[temp.length/2];
+        } else {
+            ans = (temp[temp.length/2] + temp[temp.length/2-1])/2;
+        }
+        return ans;
+    }
+}
