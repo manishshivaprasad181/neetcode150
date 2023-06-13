64b5bc30a29b65e4018ef3ab7151a38a7181f37a
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-06-13 15:18:21 +0000

    [LeetCode Sync] Runtime - 34 ms (5.11%), Memory - 54.6 MB (5.13%)

--- Leetcode problems/0200-number-of-islands/solution.java
+++ Leetcode problems/0200-number-of-islands/solution.java
@@ -1,33 +1,46 @@
 class Solution {
-    void dfs(char[][] grid, int row, int col) {
-        int nr = grid.length;
-        int nc = grid[0].length;
-
-        if(row<0||col<0||row>=nr||col>=nc||grid[row][col]=='0') {
-            return;
-        }
-        grid[row][col]='0';
-        dfs(grid, row+1, col);
-        dfs(grid, row, col+1);
-        dfs(grid, row-1, col);
-        dfs(grid, row, col-1);
-    }
     public int numIslands(char[][] grid) {
-        if(grid==null)
-            return 0;
-        int numofIslands =0;
-        int nr = grid.length;
-        int nc = grid[0].length;
-        for(int r=0;r<nr;r++) {
-            for(int c=0;c<nc;c++) {
-                if(grid[r][c]=='1')
+        if(grid.length==0)
+        return 0;
+        int rows=grid.length;
+        int cols=grid[0].length;
+        Set<Pair<Integer,Integer>> visit=new HashSet<>();
+        int islands=0;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(grid[i][j]=='1' && !visit.contains(new Pair<>(i,j)))
                 {
-                    ++numofIslands;
-                    dfs(grid, r, c);
+                    bfs(i,j,grid,visit,rows,cols);
+                    islands+=1;
                 }
+            }
+        }
+        return islands;
+    }
+    public void bfs(int i,int j,char[][] grid,Set<Pair<Integer,Integer>> visit,int rows,int cols){
+        Queue<Pair<Integer,Integer>> q=new ArrayDeque<>();
+        visit.add(new Pair<>(i,j));
+        q.add(new Pair<>(i,j));
+        while(!q.isEmpty())
+        {
+            Pair<Integer,Integer> pair=q.poll();
+            int row=pair.getKey();
+            int col=pair.getValue();
+            int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
+            for(int [] direction:directions){
+                int dr=direction[0];
+                int dc=direction[1];
+                int nr=row+dr;
+                int nc=col+dc;
+                if(nr>=0 && nr<rows && nc>=0&& nc<cols&& grid[nr][nc]=='1'&&!visit.contains(new Pair<>(nr,nc))){
+                    q.add(new Pair<>(nr,nc));
+                    visit.add(new Pair<>(nr,nc));
+                }
+            }
                 
             }
         }
-        return numofIslands;
     }
-}
+
