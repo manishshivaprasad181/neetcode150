cf3ed508eee13849a739a8ff7400a729692a971e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-06-25 22:35:31 +0000

    [LeetCode Sync] Runtime - 11 ms (35.67%), Memory - 43.9 MB (98.99%)

---
+++ Leetcode problems/1458-sort-integers-by-the-number-of-1-bits/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] sortedarr = Arrays.stream(arr).boxed().sorted((a,b)->{
+        int counta = Integer.bitCount(a);
+        int countb = Integer.bitCount(b);
+            
+            if(counta!=countb) 
+                return counta-countb;
+            else 
+                return a-b;
+            
+        }).toArray(Integer[]::new);
+        
+        return Arrays.stream(sortedarr).mapToInt(Integer::intValue).toArray();
+            
+        
+    }
+}
