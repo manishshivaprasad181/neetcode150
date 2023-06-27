b2bb7b73946b06505c8a873c65902206daa0639c
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-06-27 20:40:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (98.17%)

--- Leetcode problems/0268-missing-number/solution.java
+++ Leetcode problems/0268-missing-number/solution.java
@@ -2,12 +2,11 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
         int sum=(n*(n+1))/2;
-        int sum_of_list=0;
-        for(int i=0;i<nums.length;i++)
+        int sum_array=0;
+        for(int i:nums)
         {
-            sum_of_list+=nums[i];
+            sum_array+=i;
         }
-        return sum-sum_of_list;
-        
+        return sum-sum_array;
     }
 }
