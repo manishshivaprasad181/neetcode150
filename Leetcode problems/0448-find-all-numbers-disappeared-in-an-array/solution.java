678c2870af1d0595bf97ab1967f98c3d7e26baf7
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-06-28 15:16:38 +0000

    [LeetCode Sync] Runtime - 7 ms (35.70%), Memory - 53.4 MB (85.48%)

--- Leetcode problems/0448-find-all-numbers-disappeared-in-an-array/solution.java
+++ Leetcode problems/0448-find-all-numbers-disappeared-in-an-array/solution.java
@@ -1,36 +1,20 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        /*HashMap<Integer,Integer> hm=new HashMap<>();
-        List<Integer> l=new ArrayList<>();
-        int n=nums.length;
-        for(int i=1;i<=n;i++)
-        {
-            hm.put(i,0);
-        }
-        for(int i=0;i<n;i++)
-        {
-            
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
-            if(entry.getValue()==0)
-            l.add(entry.getKey());
-        }
-        return l;*/
+        List<Integer> res=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            int newIndex=Math.abs(nums[i])-1;
-            if(nums[newIndex]>0)
-                nums[newIndex]*=-1;
-
-        
+            if(nums[Math.abs(nums[i])-1]>0)
+            {
+                nums[Math.abs(nums[i])-1]*=-1;
+            }
         }
-        List<Integer> l=new ArrayList<>();
-        for(int i=1;i<=nums.length;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i-1]>0)
-            l.add(i);
+            if(nums[i]>0)
+            {
+                res.add(i+1);
+            }
         }
-        return l;
+        return res;
     }
 }
