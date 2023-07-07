af89f65ea89a773aed3dafda5185ddce936639af
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-07 23:19:11 +0000

    [LeetCode Sync] Runtime - 1 ms (79.66%), Memory - 40.9 MB (96.18%)

--- Leetcode problems/0165-compare-version-numbers/solution.java
+++ Leetcode problems/0165-compare-version-numbers/solution.java
@@ -3,34 +3,37 @@ class Solution {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
         
-        return compareRevisions(v1, v2, 0);   
+        return compareRevisions(v1,v2,0);
+        
     }
     private int compareRevisions(String[] v1, String[] v2, int index) {
         int num1=0;
         int num2=0;
-        if(index>= v1.length && index >= v2.length)
+        if(index>=v1.length && index>=v2.length) 
             return 0;
-        else if(index>=v1.length){
-            if(Integer.parseInt(v2[index])==0)
-                return compareRevisions(v1, v2, index+1);
-            else
+       else  if(index>= v1.length) {
+            if(Integer.parseInt(v2[index])==0) 
+                return compareRevisions(v1,v2, index+1);
+            else 
                 return -1;
-        } else if(index>=v2.length) {
+            
+        } else if(index>= v2.length) {
             if(Integer.parseInt(v1[index])==0) 
-                return compareRevisions(v1, v2, index+1);
+                return compareRevisions(v1,v2,index+1);
             else 
                 return 1;
-        } else
-             num1 = Integer.parseInt(v1[index]);
-             num2 = Integer.parseInt(v2[index]);
-        
-            if(num1<num2)
-                return -1;
-            else if(num1>num2)
+            
+        } else {
+            num1 = Integer.parseInt(v1[index]);
+            num2 = Integer.parseInt(v2[index]);
+            
+            if(num1>num2) 
                 return 1;
-        
+            else if(num1<num2)
+                return -1;
             else
-                return compareRevisions(v1,v2, index+1);
-    
+                return compareRevisions(v1, v2, index+1);
+        }
+        
     }
 }
