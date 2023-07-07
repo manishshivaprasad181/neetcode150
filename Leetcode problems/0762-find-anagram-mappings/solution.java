a027733380941896bf32989d9e2a349a14ea7209
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-07 01:42:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.2 MB (99.60%)

--- Leetcode problems/0762-find-anagram-mappings/solution.java
+++ Leetcode problems/0762-find-anagram-mappings/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] anagramMappings(int[] nums1, int[] nums2) {
         int[] mapping = new int[nums1.length];
-        Map<Integer, Integer> numToIndex = new HashMap<>();
+        Map<Integer, Integer> mapToIndex = new HashMap<>();
         
         for(int i =0;i<nums2.length;i++) {
-            numToIndex.put(nums2[i], i);
+            mapToIndex.put(nums2[i],i);
         }
-        
-        for(int i=0;i<nums1.length;i++) {
-            mapping[i]  = numToIndex.get(nums1[i]);
+        for(int i =0;i<nums1.length;i++) {
+            mapping[i] = mapToIndex.get(nums1[i]);
         }
         return mapping;
-        
     }
 }
