c3f855c18e35a9786113e88ef26a4ccbbabac349
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-07 19:41:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (31.72%)

--- Leetcode problems/1677-matrix-diagonal-sum/solution.java
+++ Leetcode problems/1677-matrix-diagonal-sum/solution.java
@@ -3,17 +3,15 @@ class Solution {
         int n = mat.length;
         int sum =0;
         
-        for(int i=0;i<n;i++) {
-            sum+=  mat[i][i];
+        for(int i =0;i<mat.length;i++) {
+            sum+= mat[i][i];
             sum+= mat[i][n-1-i];
             
         }
-        
         if(n%2==1) {
             int middle = n/2;
             sum-= mat[middle][middle];
         }
         return sum;
-        
     }
 }
