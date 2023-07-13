34c9a9763db716d953c7609a0bcccafd0fd4432a
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-13 03:14:26 +0000

    [LeetCode Sync] Runtime - 13 ms (93.96%), Memory - 47.5 MB (98.18%)

---
+++ Leetcode problems/1014-k-closest-points-to-origin/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int N = points.length;
+        int[] dists = new int[N];
+
+        for(int i =0;i<N;i++) {
+            dists[i] = dist(points[i]);
+        }
+        Arrays.sort(dists);
+        int distK = dists[k-1];
+
+        int[][] ans = new int[k][2];
+        int t=0;
+        for(int i =0;i<N;i++) {
+            if(dist(points[i])<=distK)
+                ans[t++] = points[i];
+        }
+    return ans;
+    }
+    private int dist(int[] point) {
+        return point[0]*point[0]+ point[1]*point[1];
+    }
+}
