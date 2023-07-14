0328daf5a82a70f4908ee60b4964a6c1bb7a37a4
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-14 18:43:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.9 MB (99.92%)

--- Leetcode problems/0100-same-tree/solution.java
+++ Leetcode problems/0100-same-tree/solution.java
@@ -14,50 +14,13 @@
  * }
  */
 class Solution {
-    public boolean check(TreeNode p,TreeNode q)
-    {
-        if(p==null && q==null)
-        return true;
-        if(p==null|| q==null)
-        return false;
-        if(p.val!=q.val)
-        return false;
-        return true;
-
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // if(p==null && q==null)
-        // return true;
-        // if(p==null || q==null)
-        // return false;
-        // if(p.val!=q.val)return false;
-        // return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
         if(p==null && q==null)
-        return true;
-        if(!check(p,q))return false;
-        Queue<TreeNode> queueP=new LinkedList<>();
-        Queue<TreeNode> queueQ=new LinkedList<>();
-        queueP.add(p);
-        queueQ.add(q);
-        while(!queueP.isEmpty())
-        {
-            p=queueP.poll();
-            q=queueQ.poll();
-            if(!check(p,q))return false;
-            if(p!=null)
-            {
-                if(!check(p.left,q.left))return false;
-                if(p.left!=null){
-                queueP.add(p.left);
-                queueQ.add(q.left);
-            }
-            if(!check(p.right,q.right))return false;
-            if(p.right!=null)
-            {
-                queueP.add(p.right);
-                queueQ.add(q.right);
-            }
-        } }
-        return true;
+            return true;
+        if(p==null || q==null)
+            return false;
+        if(p.val!=q.val)
+            return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
     }
 }
