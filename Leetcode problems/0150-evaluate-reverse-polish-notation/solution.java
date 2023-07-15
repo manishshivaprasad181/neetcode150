737997fd737a73a870bf45782e5dfe52b1c04183
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-15 02:06:02 +0000

    [LeetCode Sync] Runtime - 8 ms (14.80%), Memory - 43.3 MB (99.84%)

--- Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
+++ Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> s=new Stack<>();
-        for(int i=0;i<tokens.length;i++)
-        {
-            if(tokens[i].equals("+")||tokens[i].equals("*")||tokens[i].equals("/")||tokens[i].equals("-"))
-            {
-                int second=s.pop();
-                int first=s.pop();
+        Stack<Integer> s = new Stack<>();
+        for(int i =0;i<tokens.length;i++) {
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("/")|| tokens[i].equals("*")) {
+                int first = s.pop();
+                int second = s.pop();
                 if(tokens[i].equals("+"))
                     s.push(first+second);
                 else if(tokens[i].equals("-"))
-                    s.push(first-second);
+                    s.push(second-first);
                 else if(tokens[i].equals("*"))
                     s.push(first*second);
                 else
-                    s.push(first/second);
-                
-            }
-            else
+                    s.push(second/first);
+            } else
                 s.push(Integer.parseInt(tokens[i]));
         }
         return s.peek();
+
     }
 }
