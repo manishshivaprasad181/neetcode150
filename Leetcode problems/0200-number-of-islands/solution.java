20cb8d404846a104b9c5b8e59d33a23987fa9573
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-27 21:31:54 +0000

    [LeetCode Sync] Runtime - 2 ms (99.69%), Memory - 47 MB (100.00%)

--- Leetcode problems/0200-number-of-islands/solution.java
+++ Leetcode problems/0200-number-of-islands/solution.java
@@ -1,46 +1,30 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if(grid.length==0)
-        return 0;
-        int rows=grid.length;
-        int cols=grid[0].length;
-        Set<Pair<Integer,Integer>> visit=new HashSet<>();
-        int islands=0;
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-            {
-                if(grid[i][j]=='1' && !visit.contains(new Pair<>(i,j)))
-                {
-                    bfs(i,j,grid,visit,rows,cols);
-                    islands+=1;
-                }
-            }
-        }
-        return islands;
-    }
-    public void bfs(int i,int j,char[][] grid,Set<Pair<Integer,Integer>> visit,int rows,int cols){
-        Queue<Pair<Integer,Integer>> q=new ArrayDeque<>();
-        visit.add(new Pair<>(i,j));
-        q.add(new Pair<>(i,j));
-        while(!q.isEmpty())
-        {
-            Pair<Integer,Integer> pair=q.poll();
-            int row=pair.getKey();
-            int col=pair.getValue();
-            int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
-            for(int [] direction:directions){
-                int dr=direction[0];
-                int dc=direction[1];
-                int nr=row+dr;
-                int nc=col+dc;
-                if(nr>=0 && nr<rows && nc>=0&& nc<cols&& grid[nr][nc]=='1'&&!visit.contains(new Pair<>(nr,nc))){
-                    q.add(new Pair<>(nr,nc));
-                    visit.add(new Pair<>(nr,nc));
-                }
-            }
-                
-            }
-        }
-    }
+    public void dfs(char[][] grid, int i, int j) {
+        int nC = grid[0].length;
+        int nR = grid.length;
 
+        if(i<0|| i>=nR|| j<0||j>=nC|| grid[i][j]=='0')
+            return;
+
+        grid[i][j] ='0';
+        dfs(grid, i+1,j);
+        dfs(grid, i-1,j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+    }
+    public int numIslands(char[][] grid) {
+        int numCols = grid[0].length;
+        int numRows = grid.length;
+
+        int numofIslands =0;
+        for(int i=0;i<numRows;i++) {
+            for(int j=0;j<numCols;j++) {
+                if(grid[i][j]=='1') {
+                    ++numofIslands;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numofIslands;
+    }
+}
