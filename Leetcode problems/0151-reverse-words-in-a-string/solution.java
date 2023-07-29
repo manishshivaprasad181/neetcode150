e379277e82eed8a5d4f06d790e3f2d8315e2687c
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-29 23:08:24 +0000

    [LeetCode Sync] Runtime - 8 ms (54.17%), Memory - 42.2 MB (98.35%)

--- Leetcode problems/0151-reverse-words-in-a-string/solution.java
+++ Leetcode problems/0151-reverse-words-in-a-string/solution.java
@@ -1,28 +1,29 @@
 class Solution {
     public String reverseWords(String s) {
-    int left = 0;
-    int right = s.length()-1;
+        int left =0;
+        int right = s.length()-1;
 
-    while(left<=right && s.charAt(left)==' ') ++left;
+        while(left<=right && s.charAt(left)==' ') ++left;
 
-    while(left<=right && s.charAt(right) == ' ') --right;
+        while(left<=right && s.charAt(right)==' ') --right;
 
-    StringBuilder word = new StringBuilder();
-    Deque<String> d = new ArrayDeque();
+        StringBuilder word= new StringBuilder();
+        Deque<String> d = new ArrayDeque<>();
 
-    while(left<=right) {
-        char c = s.charAt(left);
-
-        if(word.length()!=0 && c==' ') {
-            d.offerFirst(word.toString());
-            word.setLength(0);
-        } else if(c!=' ') {
-            word.append(c);
+        while(left<= right) {
+            char c = s.charAt(left);
+            if(word.length()!=0 && c==' ')
+                {
+                    d.offerFirst(word.toString());
+                    word.setLength(0);
+                }
+            else if(c!=' ')
+                word.append(c);
+            
+            ++left;
         }
-        ++left;
+        d.offerFirst(word.toString());
+        return String.join(" ", d);
     }
-    d.offerFirst(word.toString());
-    return String.join(" ", d);
 
-    }
 }
