e8fa32ca2e86367d2ce1f44b298f67adf1c57cb9
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-30 15:23:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (26.99%)

--- Leetcode problems/0098-validate-binary-search-tree/solution.java
+++ Leetcode problems/0098-validate-binary-search-tree/solution.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-    private boolean validate(TreeNode root, Integer low, Integer high) {
-        if(root==null) 
+    public boolean validate(TreeNode root, Integer low, Integer high) {
+        if(root==null)
             return true;
-        if((low!=null && root.val<=low) || (high!=null && root.val>= high))
+        if(low!=null && root.val<=low || high!=null && root.val >=high)
             return false;
-
         return validate(root.right, root.val, high) && validate(root.left, low, root.val);
-
     }
-     
+    
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null, null);
+        return validate(root, null,null);
+        
     }
 }
