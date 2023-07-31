2c0971fe7537692e45e8ee0873d3c449f114dd41
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-07-31 00:24:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.56%)

--- Leetcode problems/0088-merge-sorted-array/solution.java
+++ Leetcode problems/0088-merge-sorted-array/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       int i  = m-1;
+        int i =m-1;
         int j = n-1;
         int k = m+n-1;
-        
-        while(i>=0 && j >=0) {
+
+        while(i>=0 && j>=0) {
             if(nums1[i]>nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
                 k--;
-            } else {
+            }
+            else {    
                 nums1[k] = nums2[j];
-                j--;
                 k--;
+                j--;
             }
         }
         while(j>=0) {
@@ -20,7 +21,6 @@ class Solution {
             k--;
             j--;
         }
-    
         
     }
 }
