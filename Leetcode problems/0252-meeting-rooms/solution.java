13c460554ffa56798578adfe248238999b15aa30
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-01 02:01:17 +0000

    [LeetCode Sync] Runtime - 6 ms (78.91%), Memory - 45 MB (99.46%)

--- Leetcode problems/0252-meeting-rooms/solution.java
+++ Leetcode problems/0252-meeting-rooms/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+
         int j=1;
-        for(int i=0;i<=intervals.length-2;i++)
-        {
+        for(int i=0;i<intervals.length-1;i++) {
             if(intervals[i][1]>intervals[j][0])
-            return false;
+                return false;
             j++;
         }
         return true;
