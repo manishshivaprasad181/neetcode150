a31862bad346a7ab1ca8f402c264189176b366fd
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-02 18:14:19 +0000

    [LeetCode Sync] Runtime - 5 ms (100.00%), Memory - 44.1 MB (99.45%)

--- Leetcode problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
+++ Leetcode problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
@@ -16,6 +16,6 @@ class Solution {
             return lowestCommonAncestor(root.left, p,q);
         if(p.val>root.val && q.val>root.val)
             return lowestCommonAncestor(root.right, p, q);
-        return root;
+        return root;        
     }
 }
