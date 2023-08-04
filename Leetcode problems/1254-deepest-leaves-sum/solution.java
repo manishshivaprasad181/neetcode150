913b7006118642cbf95cbb7058534970ba16331e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-04 09:27:07 +0000

    [LeetCode Sync] Runtime - 2 ms (80.03%), Memory - 45.3 MB (99.70%)

--- Leetcode problems/1254-deepest-leaves-sum/solution.java
+++ Leetcode problems/1254-deepest-leaves-sum/solution.java
@@ -15,28 +15,25 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        if(root == null)
+        if(root==null)
             return 0;
         int deepestLevel = findDeepestLevel(root);
         return calculateSum(root, 1, deepestLevel);
-        
-        
     }
     private int findDeepestLevel(TreeNode root) {
-        if(root == null)
-            return 0;
-        int leftLevel = findDeepestLevel(root.left);
-        int rightLevel = findDeepestLevel(root.right);
-        return Math.max(leftLevel, rightLevel)+1;
-        
-    }
-    private int calculateSum(TreeNode root, int currentLevel, int DeepestLevel ) {
         if(root ==null)
             return 0;
-        if(currentLevel==DeepestLevel)
-            return root.val;
-        int leftVal = calculateSum(root.left, currentLevel+1, DeepestLevel);
-        int rightVal = calculateSum(root.right, currentLevel+1, DeepestLevel);
-        return leftVal+rightVal;
+        int leftMax = findDeepestLevel(root.left);
+        int rightMax = findDeepestLevel(root.right);
+        return Math.max(leftMax, rightMax)+1;
     }
+    private int calculateSum(TreeNode root,int currentLevel, int deepestLevel) {
+        if(root==null)
+            return 0;
+        if(currentLevel==deepestLevel)
+            return root.val;
+        int left = calculateSum(root.left, currentLevel+1, deepestLevel);
+        int right =calculateSum(root.right, currentLevel+1, deepestLevel);
+        return left+right;
+    }  
 }
