8341e0a1f67ca1ca64ee64a057e6539f3493138d
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-05 20:00:00 +0000

    [LeetCode Sync] Runtime - 9 ms (97.34%), Memory - 44.6 MB (100.00%)

--- Leetcode problems/1056-capacity-to-ship-packages-within-d-days/solution.java
+++ Leetcode problems/1056-capacity-to-ship-packages-within-d-days/solution.java
@@ -1,31 +1,29 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int left=0;
-        int right =0;
-        for(int i=0;i<weights.length;i++) {
-            left = Math.max(left, weights[i]);
-            right+= weights[i];
-                
-        }
-         
-        while(left<right) {
-            int mid = (left+right)/2;
+       int left =0;
+       int right =0;
+       for(int i=0;i<weights.length;i++) {
+           left = Math.max(left, weights[i]);
+           right+= weights[i];
+       }
+
+       while(left<right) {
+           int mid = (left+right)/2;
+           int requireddays =1;
            int currentWeight =0;
-            int requireddays = 1;
-            for(int weight:weights) {
-                if(currentWeight+weight>mid) {
-                    currentWeight =0;
-                    requireddays++;
-                }
-                currentWeight+= weight;
-            }
-            if(requireddays <=days) 
+           for(int weight:weights) {
+               if(currentWeight+weight>mid) {
+                   currentWeight=0;
+                   requireddays++;
+               }
+               currentWeight+= weight;
+
+           }
+           if(requireddays<=days)
                 right = mid;
             else
-                left = mid+1;
-            
-        }
-        return left;
-        
+                left =mid+1;
+       }
+       return left;
     }
 }
