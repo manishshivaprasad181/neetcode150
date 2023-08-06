fca0bf1ca8d26598c96208fa0ec74202775733b6
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-06 00:13:23 +0000

    [LeetCode Sync] Runtime - 41 ms (78.86%), Memory - 48.5 MB (99.27%)

--- Leetcode problems/0346-moving-average-from-data-stream/solution.java
+++ Leetcode problems/0346-moving-average-from-data-stream/solution.java
@@ -1,20 +1,21 @@
 class MovingAverage {
-    private Queue<Integer> window;
     private int maxSize;
+    private Queue<Integer> window;
     private double sum;
 
     public MovingAverage(int size) {
-       window = new LinkedList<>();
-       maxSize = size;
-        sum =0.0;
+       window = new LinkedList<Integer>();
+       maxSize= size;
+       sum =0.0;
     }
     
     public double next(int val) {
-        if(window.size() == maxSize)
+        if(window.size()==maxSize)
             sum-= window.poll();
         sum+= val;
         window.offer(val);
         return sum/window.size();
+     
     }
 }
 
