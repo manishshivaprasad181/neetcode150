61407b163ab1654c1945cd3b2cf0ff5cf70aa1ce
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-06 17:28:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (35.41%)

--- Leetcode problems/0745-find-smallest-letter-greater-than-target/solution.java
+++ Leetcode problems/0745-find-smallest-letter-greater-than-target/solution.java
@@ -2,15 +2,16 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
        int left=0;
        int right=letters.length-1;
-       int mid =0;
-       while(left<=right) {
-           mid = (left+right)/2;
+       while(left<=right)
+       {
+           int mid=(left+right)/2;
            if(letters[mid]<=target)
-                left= mid+1;
-            else
-                right=mid-1;
-       }
+           {
+               left=mid+1;
+           }
+           else
+           right=mid-1;
+       } 
        return left==letters.length?letters[0]:letters[left];
     }
 }
-    
