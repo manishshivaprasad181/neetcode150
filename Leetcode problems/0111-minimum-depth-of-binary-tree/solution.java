3e5620456bb637ff926fd7758f23321a324f3526
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-07 16:24:37 +0000

    [LeetCode Sync] Runtime - 4 ms (78.63%), Memory - 62.8 MB (83.96%)

--- Leetcode problems/0111-minimum-depth-of-binary-tree/solution.java
+++ Leetcode problems/0111-minimum-depth-of-binary-tree/solution.java
@@ -15,37 +15,19 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-    //    if(root==null)
-    //    return 0;
-    //    if((root.left==null)&&(root.right==null)){
-    //        return 1;
-    //    }
-    //    int min_depth=Integer.MAX_VALUE;
-    //    if(root.left!=null)
-    //    min_depth=Math.min(minDepth(root.left),min_depth);
-    //    if(root.right!=null)
-    //    min_depth=Math.min(minDepth(root.right),min_depth);
-    //     return min_depth+1;
-        /*bfs approach*/
         if(root==null)
-        return 0;
-        LinkedList<Pair<TreeNode,Integer>> queue=new LinkedList<>();
-        queue.add(new Pair(root,1));
-        int currentdepth=0;
-        while(!queue.isEmpty())
+            return 0;
+        if(root.left==null && root.right==null)
+        return 1;
+        int min_depth=Integer.MAX_VALUE;
+        if(root.left!=null)
         {
-            Pair<TreeNode,Integer> current=queue.poll();
-            root=current.getKey();
-            currentdepth=current.getValue();
-            if((root.left==null)&&(root.right==null))
-                break;
-                if(root.left!=null)
-                queue.add(new Pair(root.left,currentdepth+1));
-                if(root.right!=null)
-                queue.add(new Pair(root.right,currentdepth+1));
-            }
-        
-        return currentdepth;
-
+            min_depth=Math.min(minDepth(root.left),min_depth);
+        }
+        if(root.right!=null)
+        {
+            min_depth=Math.min(minDepth(root.right),min_depth);
+        }
+        return min_depth+1;
     }
 }
