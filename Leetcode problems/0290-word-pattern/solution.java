5eedb81b03683ba10fcc889dbab1bd2c34ac714e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-11 00:42:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (99.57%)

--- Leetcode problems/0290-word-pattern/solution.java
+++ Leetcode problems/0290-word-pattern/solution.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if(pattern.length()!= words.length )
+       String[] words = s.split(" ");
+       Map<Character, String> map = new HashMap<>();
+       if(pattern.length()!=words.length)
             return false;
-        Map<Character,  String> map = new HashMap<>();
-        
-        for(int i =0;i<pattern.length();i++) {
-            char c = pattern.charAt(i);
+
+        for(int i=0;i<pattern.length();i++) {
+            char c  = pattern.charAt(i);
             String word = words[i];
-            if(map.containsKey(c)) {
-                if(!map.get(c).equals(word))
+
+            if(map.containsKey(c))  {
+                if(!map.get(c).equals(word)) 
                     return false;
+        
             } else {
                 if(map.containsValue(word))
                     return false;
                 map.put(c, word);
-            }
-            
+            } 
         }
         return true;
-            
-        
-    }
+    } 
 }
