3b619d4133a15292c6773180e6aaea438fab9adc
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-11 23:31:34 +0000

    [LeetCode Sync] Runtime - 273 ms (20.65%), Memory - 58.3 MB (100.00%)

--- Leetcode problems/0361-bomb-enemy/solution.java
+++ Leetcode problems/0361-bomb-enemy/solution.java
@@ -1,51 +1,49 @@
 class Solution {
     public int maxKilledEnemies(char[][] grid) {
-        if(grid.length==0)
-            return 0;
         int rows = grid.length;
         int cols = grid[0].length;
-        int hits=0;
-        
+
         int maxCount =0;
-        for(int row =0;row<rows;row++) {
-            for(int col=0;col<cols;col++) {
-                if(grid[row][col]=='0')
-                    hits = this.killEnemies(row, col, grid);
+
+        for(int i =0;i<rows;i++) {
+            for(int j =0;j<cols;j++) {
+                if(grid[i][j]=='0')   {
+                    int hits = countKillEnemies(grid, i, j);
                     maxCount = Math.max(hits, maxCount);
+                }
             }
         }
         return maxCount;
     }
-        private int killEnemies(int row, int col, char[][] grid) {
-            int enemyCount=0;
-            
-            for(int c = col-1;c>=0;c--){
-                if(grid[row][c]=='W')
-                    break;
-                else if(grid[row][c]=='E')
-                    enemyCount+=1;    
-            }
-            for(int c =col+1;c<grid[0].length;c++) {
-                if(grid[row][c]=='W')
-                    break;
-                else if(grid[row][c]=='E')
-                    enemyCount+=1;
-            }
-            for(int r =row-1;r>=0;r--) {
-                if(grid[r][col]=='W')
-                    break;
-                else if(grid[r][col]=='E')
-                    enemyCount+=1;
-            }
-            for(int r = row+1;r<grid.length;r++) {
-                if(grid[r][col]=='W')
-                    break;
-                else if(grid[r][col]=='E')
-                    enemyCount+=1;
-            }
-        
-        
-        
-       return enemyCount;
+    private int countKillEnemies(char[][] grid, int row, int col) {
+        int maxEnemies=0;
+
+        for(int c = col-1;c>=0;c--) {
+            if(grid[row][c]=='W')
+                break;
+            if(grid[row][c]=='E')
+                maxEnemies++;
+        }
+        for(int c =col+1;c<grid[0].length;c++) {
+            if(grid[row][c]=='W')
+                break;
+            if(grid[row][c] =='E')
+                maxEnemies++;
+        }
+        for(int r = row-1;r>=0;r--) {
+            if(grid[r][col]=='W')
+                break;
+            if(grid[r][col]=='E')
+                maxEnemies++;
+        }
+        for(int r=row+1;r<grid.length;r++) {
+            if(grid[r][col]=='W')
+                break;
+            if(grid[r][col]=='E')
+                maxEnemies++;
+        }
+        return maxEnemies;
     }
+       
+    
 }
