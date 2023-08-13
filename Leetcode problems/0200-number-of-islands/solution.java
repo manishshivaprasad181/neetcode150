78cee7ccabd19c083b8cd2dfbe7f3b7fb2bb13be
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-08-13 00:37:24 +0000

    [LeetCode Sync] Runtime - 2 ms (99.69%), Memory - 47.6 MB (100.00%)

--- Leetcode problems/0200-number-of-islands/solution.java
+++ Leetcode problems/0200-number-of-islands/solution.java
@@ -1,30 +1,31 @@
 class Solution {
     public void dfs(char[][] grid, int i, int j) {
-        int nC = grid[0].length;
-        int nR = grid.length;
-
-        if(i<0|| i>=nR|| j<0||j>=nC|| grid[i][j]=='0')
+        if(grid.length==0)
             return;
-
-        grid[i][j] ='0';
-        dfs(grid, i+1,j);
-        dfs(grid, i-1,j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(i>=0 && i<rows&& j>=0 && j<cols && grid[i][j]!='0') {
+            grid[i][j]= '0';
+            dfs(grid, i+1,j);
+            dfs(grid, i-1, j);
+            dfs(grid, i, j+1);
+            dfs(grid, i, j-1);
+        }
     }
+  
     public int numIslands(char[][] grid) {
-        int numCols = grid[0].length;
-        int numRows = grid.length;
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        int numofIslands =0;
-        for(int i=0;i<numRows;i++) {
-            for(int j=0;j<numCols;j++) {
+        int numIslands =0;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
                 if(grid[i][j]=='1') {
-                    ++numofIslands;
-                    dfs(grid, i, j);
+                    ++numIslands;
+                    dfs(grid,i,j);
                 }
             }
         }
-        return numofIslands;
+        return numIslands;
     }
 }
