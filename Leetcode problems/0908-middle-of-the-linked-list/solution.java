0f5980c464e9b68f63ba1088afe32086a25f51c4
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-09-05 00:44:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.9 MB (99.94%)

--- Leetcode problems/0908-middle-of-the-linked-list/solution.java
+++ Leetcode problems/0908-middle-of-the-linked-list/solution.java
@@ -10,13 +10,12 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode fast = head;
-        ListNode slow= head;
-
-        while(fast!=null&& fast.next!=null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
         }
         return slow;
     }
