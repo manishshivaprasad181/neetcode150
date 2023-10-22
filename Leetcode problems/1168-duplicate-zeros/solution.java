8261377be0d35626c6dae1164b3ee3af65510b6a
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-22 15:33:58 +0000

    [LeetCode Sync] Runtime - 1 ms (97.35%), Memory - 43.7 MB (99.65%)

--- Leetcode problems/1168-duplicate-zeros/solution.java
+++ Leetcode problems/1168-duplicate-zeros/solution.java
@@ -1,27 +1,26 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-       int length=arr.length;
-       int zeros=0;
-       for(int i=0;i<length;i++)
-       {
-           if(arr[i]==0)
-            zeros++;
-       }
-       int lastindex=length-1;
-       int newindex=length-1+zeros;
-       while(lastindex>=0)
-       {
-           if(newindex<length)
-           {
-               arr[newindex]=arr[lastindex];
-           }
-           if(arr[lastindex]==0){
-               newindex--;
-            if(newindex<length)
-            arr[newindex]=0;
-           }
-           newindex--;
-           lastindex--;
-       }
+       int n=arr.length;
+        int zeros=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+                zeros++;
+        }
+      int lastindex=n-1;
+        int newindex=n-1+zeros;
+        while(lastindex>=0)
+        {
+            if(newindex<n){
+                arr[newindex]=arr[lastindex];
+            }
+            if(arr[lastindex]==0){
+                newindex--;
+            if(newindex<n)
+                arr[newindex]=0;
+            }
+            lastindex--;
+            newindex--;
+        }
     }
 }
