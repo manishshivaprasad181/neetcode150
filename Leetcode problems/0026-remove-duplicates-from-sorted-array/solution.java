4241fce60807d134e31f41c2392669ce1578ccf1
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-23 18:09:48 +0000

    [LeetCode Sync] Runtime - 1 ms (73.84%), Memory - 43.6 MB (99.99%)

--- Leetcode problems/0026-remove-duplicates-from-sorted-array/solution.java
+++ Leetcode problems/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int insertIndex=1;
-        for(int i=1;i<nums.length;i++)
+        int n=nums.length;
+        int insertindex=1;
+        for(int i=1;i<n;i++)
         {
-            if(nums[i]!=nums[i-1])
+            if(nums[i-1]!=nums[i])
             {
-                nums[insertIndex]=nums[i];
-                insertIndex++;
-
+                nums[insertindex]=nums[i];
+                insertindex++;
             }
         }
-        return insertIndex;
+        return insertindex;
     }
 }
