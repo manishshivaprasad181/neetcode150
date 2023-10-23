bdc687f5c53d7bc9807d4e2bbfa23243440e1196
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-23 03:21:08 +0000

    [LeetCode Sync] Runtime - 1 ms (97.97%), Memory - 42.9 MB (42.35%)

--- Leetcode problems/1468-check-if-n-and-its-double-exist/solution.java
+++ Leetcode problems/1468-check-if-n-and-its-double-exist/solution.java
@@ -1,16 +1,14 @@
-import java.util.*;
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Hashtable<Integer,Integer> hs=new Hashtable<>();
-        for(int i=0;i<arr.length;i++){
-            int mul=arr[i]*2;
-            int div=arr[i]/2;
-            if(hs.containsKey(mul)||hs.containsKey(div)&& arr[i]%2==0)
-                return true;
+        HashSet<Integer> hs=new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(hs.contains(arr[i]*2)||hs.contains(arr[i]/2)&&arr[i]%2==0)
+            return true;
             else
-                hs.put(arr[i],i);
+            hs.add(arr[i]);
+
         }
-                                                            
         return false;
     }
 }
