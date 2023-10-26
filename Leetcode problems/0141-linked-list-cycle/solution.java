63ce1cf173f8a19328234e80d923af83b8b9a5ae
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-26 15:48:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (98.56%)

--- Leetcode problems/0141-linked-list-cycle/solution.java
+++ Leetcode problems/0141-linked-list-cycle/solution.java
@@ -11,15 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-       Set<ListNode> nodeSeen = new HashSet<>();
-       while(head!=null) {
-           if(nodeSeen.contains(head))
-            return true;
-            nodeSeen.add(head);
-            head = head.next;
-       }
-       return false;
+        if(head==null)
+            return false;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(slow!=fast)
+        {
+            if(fast==null || fast.next==null)
+                return false;
+            slow=slow.next;
+        fast=fast.next.next;
+        }
+   return true;
+        
+        
     }
+    
 }
-
-
