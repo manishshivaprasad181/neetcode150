542198ace2d9275f594a94cfb06bd86b6c85100f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-29 21:20:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (99.72%)

--- Leetcode problems/0328-odd-even-linked-list/solution.java
+++ Leetcode problems/0328-odd-even-linked-list/solution.java
@@ -10,19 +10,21 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null) return null;
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenhead = even ;
-        while(even!=null &&even.next !=null) {
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
-            even = even.next;
+       if(head==null)
+       return null;
+    
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenhead=even;
+        while(even!=null && even.next!=null)
+        {
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
 
         }
-        odd.next = evenhead;
+        odd.next=evenhead;
         return head;
-        
     }
 }
