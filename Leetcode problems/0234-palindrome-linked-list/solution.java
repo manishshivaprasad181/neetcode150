4767b565df860e36642bb15e10b9180462af3635
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-10-29 22:01:11 +0000

    [LeetCode Sync] Runtime - 6 ms (51.49%), Memory - 60.4 MB (62.69%)

--- Leetcode problems/0234-palindrome-linked-list/solution.java
+++ Leetcode problems/0234-palindrome-linked-list/solution.java
@@ -10,22 +10,46 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       List<Integer> val = new ArrayList<>();
-
-       ListNode curr = head;
-       while(curr!=null) {
-           val.add(curr.val);
-           curr = curr.next;
-       }
-
-       int front =0;
-       int back = val.size()-1;
-       for(int i=0;i<val.size();i++) {
-           if(!val.get(front).equals(val.get(back))) 
-                return false;
-            front++;
-            back--;
-       }
+        if(head==null)
         return true;
+        ListNode firsthalfend=endoffirsthalf(head);
+        ListNode secondhalfstart=reversesecondhalf(firsthalfend);
+        ListNode p2=secondhalfstart;
+        ListNode p1=head;
+        while(p2!=null)
+        {
+            if(p2.val!=p1.val)
+            return false;
+            else
+            {
+                p2=p2.next;
+                p1=p1.next;
+            }
+        }
+        return true;
+    }
+    public ListNode reversesecondhalf(ListNode head)
+    {
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null)
+        {
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    public ListNode endoffirsthalf(ListNode head)
+    {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
