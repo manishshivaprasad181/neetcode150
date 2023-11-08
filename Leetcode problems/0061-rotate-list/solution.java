a8f143d44db695c12166852fdbf84852388e5658
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-11-08 19:33:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (99.77%)

--- Leetcode problems/0061-rotate-list/solution.java
+++ Leetcode problems/0061-rotate-list/solution.java
@@ -10,21 +10,26 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head ==null)
+        if(head==null)
             return null;
-        ListNode old_tail= head;
-        int n;
-        for(n=1;old_tail.next!=null;n++)
-            old_tail = old_tail.next;
-        old_tail.next = head;
-
-        ListNode new_tail = head;
-        for(int i=0;i<n-k%n-1;i++) 
-            new_tail = new_tail.next;
-        ListNode new_head = new_tail.next;
-        new_tail.next = null;
-
-        return new_head;
-        
+        int length=1;
+        ListNode tail=head;
+        while(tail.next!=null)
+        {
+            length++;
+            tail=tail.next;
+        }
+        k=k%length;
+        if(k==0)
+            return head;
+        ListNode curr=head;
+        for(int i=0;i<length-k-1;i++)
+        {
+            curr=curr.next;
+        }
+        ListNode newhead=curr.next;
+        curr.next=null;
+        tail.next=head;
+        return newhead;
     }
 }
