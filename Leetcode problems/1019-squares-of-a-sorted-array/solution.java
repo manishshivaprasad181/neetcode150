74949c33beb64bdf0fe193f9846299e2cc048702
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-11-13 23:48:18 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.9 MB (99.68%)

--- Leetcode problems/1019-squares-of-a-sorted-array/solution.java
+++ Leetcode problems/1019-squares-of-a-sorted-array/solution.java
@@ -3,20 +3,21 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         int k=nums.length-1;
-        int [] result=new int[nums.length];
-        while(right>=left)
+        int[] result=new int[nums.length];
+        while(left<=right)
         {
-            if(Math.abs(nums[left])>Math.abs(nums[right]))
-            {
-                result[k--]=nums[left]*nums[left];
-                left++;
-            }
-            else
-            {
-                result[k--]=nums[right]*nums[right];
-                right--;
-            }
+            if(Math.abs(nums[right])>Math.abs(nums[left]))
+           { 
+               result[k--]=nums[right]*nums[right];
+               right--;
+           }
+           else
+           {
+               result[k--]=nums[left]*nums[left];
+               left++;
+           }
         }
         return result;
+
     }
 }
