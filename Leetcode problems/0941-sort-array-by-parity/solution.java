91d8b0b93968128aa211bd1f8d0dd42158bd138b
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-11-21 19:02:47 +0000

    [LeetCode Sync] Runtime - 1 ms (60.07%), Memory - 43.6 MB (99.66%)

--- Leetcode problems/0941-sort-array-by-parity/solution.java
+++ Leetcode problems/0941-sort-array-by-parity/solution.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int start=0;
-        int end=nums.length-1;
-        while(start<end)
-        {
-            while(start<end && nums[start]%2==0)
-                start++;
-            while(start<end && nums[end]%2==1)
-                end--;
-            swap(start,end,nums);
-            start++;
-            end--;
-        }
-        return nums;
-    }
-    public void swap(int start,int end, int[] nums)
-    {
-        int temp=nums[start];
-        nums[start]=nums[end];
-        nums[end]=temp;
+      int k=0;
+      for(int i=0;i<nums.length;i++)
+      {
+          if(nums[i]%2==0)
+          {
+              int temp=nums[k];
+              nums[k]=nums[i];
+              nums[i]=temp;
+              k++;
+          }
+      }  
+      return nums;
     }
 }
