9f4d8d1939b6799ba12b0872d429817a53098bd1
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-01-22 10:15:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (55.20%)

--- Leetcode problems/0054-spiral-matrix/solution.java
+++ Leetcode problems/0054-spiral-matrix/solution.java
@@ -1,32 +1,34 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
       List<Integer> result = new ArrayList<>();
-      int rows = matrix.length;
-      int cols = matrix[0].length;
-      int up =0;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length; 
+      int up = 0;
       int left =0;
       int right = matrix[0].length-1;
       int down = matrix.length-1;
 
-      while(result.size()<rows*cols) {
-        for(int col =left;col<=right;col++)
-          result.add(matrix[up][col]);
-        for(int row = up+1;row<=down;row++) 
-          result.add(matrix[row][right]);
-        if(up!=down) {
-            for(int col = right-1;col>=left;col--)
-              result.add(matrix[down][col]);
-        }
-        if(left!=right) {
-          for(int row= down-1;row>up;row--) 
-            result.add(matrix[row][left]);
-        }
-        up++;
-        down--;
-        left++;
-        right--;
-      }
-      return result;
+        while(result.size()< rows*cols) {
+            for(int col= left;col<=right;col++) 
+                result.add(matrix[up][col]);
+            for(int row = up+1;row<=down;row++) 
+                result.add(matrix[row][right]);
+            
+            if(up!=down) {
+                for(int col =right-1;col>=left;col--) 
+                    result.add(matrix[down][col]);
+            }
 
+            if(left!=right) {
+                for(int row= down-1;row>up;row--) 
+                    result.add(matrix[row][left]);
+            }
+            up++;
+            down--;
+            left++;
+            right--;
+        }
+        return result;
     }
 }
