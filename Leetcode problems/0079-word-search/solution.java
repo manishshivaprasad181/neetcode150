7f8a84f771dd6e50863f547865f39e354a6b80a6
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-01-22 09:31:39 +0000

    [LeetCode Sync] Runtime - 215 ms (16.30%), Memory - 45 MB (12.75%)

--- Leetcode problems/0079-word-search/solution.java
+++ Leetcode problems/0079-word-search/solution.java
@@ -4,35 +4,39 @@ class Solution {
     private int cols;
 
     public boolean exist(char[][] board, String word) {
-        this.board = board;
+        this.board =board;
         this.rows = board.length;
         this.cols = board[0].length;
 
         for(int i =0;i<rows;i++) {
             for(int j =0;j<cols;j++) {
-                if(backtrack(i, j, word,0))
-                    return true; 
-            
+                if(backtrack(i, j, word, 0))
+                    return true;
             }
-
         }
-        return false;   
+        return false;
     }
-        private boolean backtrack(int row, int col, String word, int index) {
-            if(index>= word.length())
-                return true;
-            if(row<0||row>=rows||col<0||col>=cols||word.charAt(index)!=board[row][col]) 
+
+    private boolean backtrack(int row, int col, String word, int index) {
+        if(index>=word.length())
+            return true;
+        if(row<0 || row>=rows ||col<0 ||col>=cols || word.charAt(index)!= board[row][col])
             return false;
-            boolean ret= false;
-            this.board[row][col] ='#';
-            int[] rowoffset = {0,1,-1,0};
-            int[] coloffset = {1, 0,0,-1};
-            for(int i =0;i<4;i++) {
-                ret = backtrack(row+rowoffset[i], col+coloffset[i],word, index+1);
+        
+        boolean ret = false;
+        this.board[row][col] = '#';
+
+        int[] rowoffset = {0,1,-1,0};
+        int[] coloffset = {1, 0, 0, -1};
+
+       for(int i=0;i<4;i++) {
+             ret = backtrack(row+rowoffset[i], col+coloffset[i], word, index+1);
                 if(ret)
                     break;
-            }
-            this.board[row][col] = word.charAt(index);
-            return ret;
+            
         }
+        this.board[row][col] = word.charAt(index);
+        return ret;
+
+    }
 }
