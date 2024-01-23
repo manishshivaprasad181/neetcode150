25fd91d95e529a1aae3908b300e56482ad957281
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-01-23 08:00:08 +0000

    [LeetCode Sync] Runtime - 3 ms (60.20%), Memory - 43.7 MB (98.77%)

--- Leetcode problems/0036-valid-sudoku/solution.java
+++ Leetcode problems/0036-valid-sudoku/solution.java
@@ -1,32 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int N=9;
-        HashSet<Character>[] rows=new HashSet[N];
-        HashSet<Character>[] cols=new HashSet[N];
-        HashSet<Character>[] boxes=new HashSet[N];
-        for(int r=0;r<N;r++)
-        {
-            rows[r]=new HashSet<Character>();
-            cols[r]=new HashSet<Character>();
-            boxes[r]=new HashSet<Character>();
-        }
-        for(int r=0;r<N;r++){
-            for(int c=0;c<N;c++){
-                char val=board[r][c];
-                if(val=='.')
-                continue;
-                if(rows[r].contains(val))
-                return false;
-                rows[r].add(val);
-                if(cols[c].contains(val))
-                return false;
-                cols[c].add(val);
-                int idx=(r/3)*3+c/3;
+       int N =9;
+
+       HashSet<Character>[] rows = new HashSet[N];
+       HashSet<Character>[] cols = new HashSet[N];
+       HashSet<Character>[] boxes = new HashSet[N];
+
+       for(int i=0;i<N;i++) {
+           rows[i] = new HashSet<Character>();
+           cols[i] = new HashSet<Character>();
+           boxes[i] = new HashSet<Character>();
+       }
+
+       for(int row=0;row<N;row++) {
+           for(int col=0;col<N;col++) {
+               if(board[row][col]=='.')
+                    continue;
+                char val = board[row][col];
+                if(rows[row].contains(val))
+                    return false;
+                rows[row].add(val);
+                if(cols[col].contains(val))
+                    return false;
+                cols[col].add(val);
+
+                int idx = (row/3)*3+col/3;
                 if(boxes[idx].contains(val))
-                return false;
+                    return false;
                 boxes[idx].add(val);
-            }
-        }
-        return true;
+
+           }
+       }
+       return true;
     }
 }
