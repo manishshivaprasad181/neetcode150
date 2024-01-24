b21f4e36060cf62f4c12b7eb9526a92f3b1fda21
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-01-24 20:27:20 +0000

    [LeetCode Sync] Runtime - 8 ms (14.80%), Memory - 44.9 MB (15.41%)

--- Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
+++ Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> s = new Stack<>();
-        for(int i =0;i<tokens.length;i++) {
-            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("/")|| tokens[i].equals("*")) {
-                int first = s.pop();
-                int second = s.pop();
-                if(tokens[i].equals("+"))
-                    s.push(first+second);
-                else if(tokens[i].equals("-"))
-                    s.push(second-first);
-                else if(tokens[i].equals("*"))
-                    s.push(first*second);
-                else
-                    s.push(second/first);
-            } else
-                s.push(Integer.parseInt(tokens[i]));
-        }
-        return s.peek();
+       Stack<Integer> st = new Stack<>();
 
+       for(int i=0;i<tokens.length;i++) {
+           if(tokens[i].equals("+")||tokens[i].equals("-")||tokens[i].equals("*")||tokens[i].equals("/")) {
+               int first = st.pop();
+               int second = st.pop();
+
+                if(tokens[i].equals("+"))
+                    st.push(first+second);
+                else if(tokens[i].equals("-"))
+                    st.push(second-first);
+                else if(tokens[i].equals("*"))
+                    st.push(first*second);
+                else
+                    st.push(second/first);
+           } else
+                st.push(Integer.parseInt(tokens[i]));
+    
+       }
+       return st.peek();
     }
 }
