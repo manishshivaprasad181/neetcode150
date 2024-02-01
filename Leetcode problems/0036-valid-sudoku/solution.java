c0b66224fa00a656e9fd651b62ff534fd0e2eeb7
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-01 17:50:21 +0000

    [LeetCode Sync] Runtime - 14 ms (23.21%), Memory - 44.9 MB (15.62%)

--- Leetcode problems/0036-valid-sudoku/solution.java
+++ Leetcode problems/0036-valid-sudoku/solution.java
@@ -1,36 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-       int N =9;
-
-       HashSet<Character>[] rows = new HashSet[N];
-       HashSet<Character>[] cols = new HashSet[N];
-       HashSet<Character>[] boxes = new HashSet[N];
-
-       for(int i=0;i<N;i++) {
-           rows[i] = new HashSet<Character>();
-           cols[i] = new HashSet<Character>();
-           boxes[i] = new HashSet<Character>();
-       }
-
-       for(int row=0;row<N;row++) {
-           for(int col=0;col<N;col++) {
-               if(board[row][col]=='.')
-                    continue;
-                char val = board[row][col];
-                if(rows[row].contains(val))
-                    return false;
-                rows[row].add(val);
-                if(cols[col].contains(val))
-                    return false;
-                cols[col].add(val);
-
-                int idx = (row/3)*3+col/3;
-                if(boxes[idx].contains(val))
-                    return false;
-                boxes[idx].add(val);
-
-           }
-       }
-       return true;
+        Set<String> seen=new HashSet<>();
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char number=board[i][j];
+                if(number!='.')
+                if(!seen.add(number+"in row"+i)||
+                !seen.add(number+"in column"+j)||
+                !seen.add(number+"in block"+i/3+"-"+j/3))
+                return false;
+            }
+        }
+        return true;
     }
 }
