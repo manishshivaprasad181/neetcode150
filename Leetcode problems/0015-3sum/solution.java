5ab02c098c0d4bdb84dfdd488de9aca23de8d869
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-02 18:44:48 +0000

    [LeetCode Sync] Runtime - 26 ms (93.47%), Memory - 51.4 MB (60.78%)

--- Leetcode problems/0015-3sum/solution.java
+++ Leetcode problems/0015-3sum/solution.java
@@ -1,27 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
        Arrays.sort(nums);
-       List<List<Integer>> res = new ArrayList<>();
-       for(int i =0;i<nums.length;i++){
-           if(i==0|| nums[i-1]!=nums[i]) 
-                twosum(nums, i, res);
+       List<List<Integer>> res=new ArrayList<>();
+       for(int i=0;i<nums.length;i++)
+       {
+           if(nums[i]>0)
+           break;
+           if(i>0 && nums[i]==nums[i-1])
+           continue;
+           
+           int l=i+1, r=nums.length-1;
+           while(l<r)
+           {
+               int sum=nums[i]+nums[l]+nums[r];
+               if(sum>0)
+               r--;
+               else if(sum<0)
+               l++;
+               else
+               {
+                   res.add(Arrays.asList(nums[i], nums[l],nums[r]));
+                   l++;
+                   r--;
+                   while(l<r && nums[l]==nums[l-1])
+                   l++;
+
+               }
+           }
+
+           
        }
-    return res;
-    }
-    void twosum(int[] nums, int i , List<List<Integer>> res) {
-        int lo =i+1;
-        int hi = nums.length-1;
-        while(lo<hi) {
-            int sum = nums[i] + nums[lo]+ nums[hi];
-            if(sum<0)
-                ++lo;
-            else if(sum>0)
-                --hi;
-            else {
-                res.add(Arrays.asList(nums[i],nums[lo++],nums[hi--]));
-                while(lo<hi &&nums[lo-1] == nums[lo])
-                    ++lo;
-            }
-        }
+       return res;
+
+
     }
 }
