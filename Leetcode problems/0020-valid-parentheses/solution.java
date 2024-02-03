efcceb27298dcc62fdf68c91cd62dcfe7a20369b
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-03 22:23:52 +0000

    [LeetCode Sync] Runtime - 2 ms (54.03%), Memory - 41.5 MB (44.76%)

--- Leetcode problems/0020-valid-parentheses/solution.java
+++ Leetcode problems/0020-valid-parentheses/solution.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-
-        HashMap<Character, Character> hs = new HashMap<>();
-        hs.put('}', '{');
-        hs.put(')', '(');
-        hs.put(']', '[');
-        for(int i =0;i<s.length();i++) {
-            if(hs.containsKey(s.charAt(i))) {
-                if(!st.isEmpty() && st.peek() == hs.get(s.charAt(i)))
+        Stack<Character> st=new Stack<>();
+        HashMap<Character,Character> hm=new HashMap<>();
+        hm.put('}','{');
+        hm.put(')','(');
+        hm.put(']','[');
+        for(int i=0;i<s.length();i++)
+        {
+            if(hm.containsKey(s.charAt(i)))
+            {
+                if(!st.isEmpty()&&st.peek()==hm.get(s.charAt(i)))
+                {
                     st.pop();
+                }
                 else
-                    return false;
+                return false;
             }
             else
-                st.push(s.charAt(i));
+            st.push(s.charAt(i));
         }
         return st.isEmpty();
-
+        
     }
 }
