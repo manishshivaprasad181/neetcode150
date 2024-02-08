511abea8c9c215b4daa7868bbe411436dcdde0bd
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-08 01:42:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (16.13%)

--- Leetcode problems/0074-search-a-2d-matrix/solution.java
+++ Leetcode problems/0074-search-a-2d-matrix/solution.java
@@ -4,19 +4,20 @@ class Solution {
         int n = matrix[0].length;
         if(m==0)
             return false;
-        
-        int low =0;
+        int midelement = 0;
+        int low = 0;
         int high = m*n-1;
-        int midelement;
-        while(low<=high) {
-            int mid = (high+low)/2;
+        while(low<= high) {
+            int mid = low+(high-low)/2;
             midelement = matrix[mid/n][mid%n];
-            if(midelement==target)
+
+            if(midelement == target)
                 return true;
-            if(midelement<target)
+            if(midelement<target) 
                 low= mid+1;
             else
                 high = mid-1;
+
         }
         return false;
     }
