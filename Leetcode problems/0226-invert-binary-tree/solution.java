a3848aab954563dd04cc4f91e4ab9d2508e3aad6
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-16 07:36:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (84.57%)

--- Leetcode problems/0226-invert-binary-tree/solution.java
+++ Leetcode problems/0226-invert-binary-tree/solution.java
@@ -15,13 +15,12 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-       if(root ==null)
-        return null;
-
+        if(root ==null)
+            return null;
         TreeNode right = invertTree(root.right);
         TreeNode left = invertTree(root.left);
-        root.right = left;
         root.left = right;
+        root.right  = left;
         return root;
     }
 }
