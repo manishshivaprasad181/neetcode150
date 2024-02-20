f396686edd9d23798fbc4b002600a8c151221628
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-20 21:12:40 +0000

    [LeetCode Sync] Runtime - 2 ms (64.47%), Memory - 45.9 MB (51.02%)

--- Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
+++ Leetcode problems/0004-median-of-two-sorted-arrays/solution.java
@@ -1,40 +1,47 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int s1 = nums1.length;
-        int s2 = nums2.length;
-        double[] temp = new double[s1+s2];
+        int s1=nums1.length;
+        int s2=nums2.length;
         int i=0,j=0,k=0;
-        while(i<s1&&j<s2) {
-            if(nums1[i]<nums2[j]) {
-                temp[k] = nums1[i];
-                k++;
-                i++;
-            } else {
-                temp[k] = nums2[j];
-                k++;
+        double[] res=new double[s1+s2];
+        while(i<s1 && j<s2)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                res[k++]=nums2[j];
                 j++;
-            }
-        }
-        if(i>=s1) {
-            for(int m =j;m<s2;m++) {
-                temp[k] = nums2[m];
-                k++;
-            }
-        }
-        if(j>=s2) {
-            for(int n =i;n<s1;n++) {
-                temp[k] = nums1[n];
-                k++;
-            }
-        }
-        double ans=0.0;
-        if(temp.length%2!=0) {
-            ans = temp[temp.length/2];
 
-        }  else {
-            ans = (temp[temp.length/2] +temp[temp.length/2-1])/2;
+            }
+            else
+            {
+                res[k++]=nums1[i];
+                i++;
+            }
+
         }
-        return ans;
+        if(i>=s1)
+        {
+        for(int m=j;m<s2;m++)
+        {
+            res[k++]=nums2[m];
+        }
+        }
+         if(j>=s2)
+        {
+        for(int m=i;m<s1;m++)
+        {
+            res[k++]=nums1[m];
+        }
+        }
+
+        if(res.length%2!=0)
+        {
+            return res[res.length/2];
+        }
+        else
+        {
+            return (res[res.length/2]+res[res.length/2-1])/2;
+        }
 
     }
 }
