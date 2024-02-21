4058dc39962d806cb63a9ad91f27241a2490e1ef
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-21 00:06:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (76.51%)

--- Leetcode problems/0066-plus-one/solution.java
+++ Leetcode problems/0066-plus-one/solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int len=digits.length-1;
-        for(int i=len;i>=0;i--)
-        {
+        int len = digits.length-1;
+        for(int i=len;i>=0;i--) {
             if(digits[i]==9)
                 digits[i]=0;
-            else
-            {
+            else {
                 digits[i]++;
                 return digits;
             }
         }
-        digits=new int[len+2];
-        digits[0]=1;
+        digits = new int[len+2];
+        digits[0] =1;
         return digits;
-
         
         
     }
