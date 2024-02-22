6e9a6fdfc6363d2dafb654e31e1f3863c1691513
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-22 22:55:25 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 52.5 MB (24.89%)

---
+++ Leetcode problems/1544-count-good-nodes-in-binary-tree/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int numGoodNodes=0;
+    public int goodNodes(TreeNode root) {
+      dfs(root, Integer.MIN_VALUE);
+      return numGoodNodes;
+    }
+    private void dfs(TreeNode node, int max) {
+        if(max<=node.val)
+            numGoodNodes++;
+        if(node.left!=null) 
+            dfs(node.left, Math.max(node.val, max));
+        if(node.right!=null)
+            dfs(node.right, Math.max(node.val, max));
+    }
+}
