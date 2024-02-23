5b858316be4382f1dc0b51417c5559bee1034bdf
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-23 23:58:56 +0000

    [LeetCode Sync] Runtime - 2 ms (96.27%), Memory - 44.5 MB (29.56%)

--- Leetcode problems/0572-subtree-of-another-tree/solution.java
+++ Leetcode problems/0572-subtree-of-another-tree/solution.java
@@ -22,7 +22,7 @@ class Solution {
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
     public boolean isIdentical(TreeNode node1, TreeNode node2) {
-        if(node1==null|| node2==null)
+        if(node1==null || node2==null)
             return (node1==null && node2==null);
         return (node1.val==node2.val) && isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right);
     }
