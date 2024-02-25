740a7bdd0a9b1c0d3124792bad096ddc3df89c1c
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-25 00:35:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (42.88%)

--- Leetcode problems/0019-remove-nth-node-from-end-of-list/solution.java
+++ Leetcode problems/0019-remove-nth-node-from-end-of-list/solution.java
@@ -10,27 +10,19 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        int length=0;
-        ListNode first =head;
-        while(first!=null)
-        {
-        length++;
-        first=first.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second= dummy;
+
+        for(int i=1;i<=n+1;i++) {
+            first = first.next;
         }
-        System.out.println(length);
-        length=length-n;
-        first=dummy;
-        int i=0;
-        while(i<length)
-        {
-            i++;
+        while(first!=null) {
             first=first.next;
+            second = second.next;
         }
-        first.next=first.next.next;
+        second.next = second.next.next;
         return dummy.next;
-
-
     }
 }
