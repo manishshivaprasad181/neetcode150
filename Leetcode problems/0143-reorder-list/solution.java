d64118823385eb6b77338d29d99515643aebafa2
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-25 01:21:12 +0000

    [LeetCode Sync] Runtime - 2 ms (90.30%), Memory - 48.1 MB (56.49%)

---
+++ Leetcode problems/0143-reorder-list/solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null) return;
+
+        ListNode fast =head;
+        ListNode slow=head;
+
+        while(fast!=null && fast.next !=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev=null;
+        ListNode curr = slow, tmp;
+        while(curr!=null) {
+            tmp = curr.next;
+            curr.next = prev;
+            prev =curr;
+            curr = tmp;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+        
+        while(second.next!=null) {
+            tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+         
+        
+    }
+}
