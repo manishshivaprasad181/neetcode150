a949fc255fdd869e032175ee42d1475ea1c6965b
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-26 05:58:39 +0000

    [LeetCode Sync] Runtime - 6 ms (74.79%), Memory - 44.4 MB (64.31%)

--- Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
+++ Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,42 +1,34 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-       Stack<Integer> st=new Stack<>();
-       HashSet<String> hs=new HashSet<>();
-       hs.add("+");
-       hs.add("*");
-       hs.add("/");
-       hs.add("-");
-       for(int i=0;i<tokens.length;i++)
-       {
-           if(!hs.contains(tokens[i]))
-           {
-               st.push(Integer.parseInt(tokens[i]));
-           }
-           else
-           {
-               if(tokens[i].equals("+"))
-               {
-                   st.push(st.pop()+st.pop());
-               }
-               else if(tokens[i].equals("-"))
-               {
-                   int x=st.pop();
-                   int y=st.pop();
-                   st.push(y-x);
-               }
-               else if(tokens[i].equals("*"))
-               {
-                   st.push(st.pop()*st.pop());
+        Stack<Integer> st = new Stack<>();
+        HashSet<String> hs= new HashSet<>();
+        hs.add("+");
+        hs.add("-");
+        hs.add("*");
+        hs.add("/");
 
-               }
-               else
-               {
-                   int x=st.pop();
-                   int y=st.pop();
-                   st.push(y/x);
-               }
-           }
-       }
-       return st.pop(); 
+        for(int i=0;i<tokens.length;i++) {
+            if(!hs.contains(tokens[i])) 
+                st.push(Integer.parseInt(tokens[i]));
+            else {
+                if(tokens[i].equals("+")) 
+                    st.push(st.pop()+st.pop());
+                else if(tokens[i].equals("-")) {
+                    int x = st.pop();
+                    int y = st.pop();
+                    st.push(y-x);
+                } else if(tokens[i].equals("*")) 
+                    st.push(st.pop()*st.pop());
+                else {
+                    int x = st.pop();
+                    int y = st.pop();
+                    st.push(y/x);
+                }
+            }
+
+            
+        }
+        return st.pop();
+        
     }
 }
