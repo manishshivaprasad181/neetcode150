977686b93604afa817331922092cdf4e851f3deb
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-26 18:05:54 +0000

    [LeetCode Sync] Runtime - 5 ms (53.40%), Memory - 42.4 MB (98.03%)

--- Leetcode problems/0242-valid-anagram/solution.java
+++ Leetcode problems/0242-valid-anagram/solution.java
@@ -4,18 +4,14 @@ class Solution {
             return false;
 
         int[] counter = new int[26];
-
         for(int i=0;i<s.length();i++) {
             counter[s.charAt(i)-'a']++;
             counter[t.charAt(i)-'a']--;
-
-        }   
-
+        }
         for(int i =0;i<26;i++) {
             if(counter[i]!=0)
                 return false;
-        }    
+        }
         return true;
-    
     }
 }
