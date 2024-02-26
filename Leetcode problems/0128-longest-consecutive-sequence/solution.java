8024a908b23800cdb3d7eddb17eb07d4ba93cbfe
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-26 23:12:05 +0000

    [LeetCode Sync] Runtime - 1106 ms (18.58%), Memory - 65.8 MB (47.56%)

--- Leetcode problems/0128-longest-consecutive-sequence/solution.java
+++ Leetcode problems/0128-longest-consecutive-sequence/solution.java
@@ -1,24 +1,20 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> numset=new HashSet<>();
-        int longest=0;
+        HashSet<Integer> numset = new HashSet<>();
+
         for(int num:nums)
-        {
             numset.add(num);
-        }
-        for(int num:nums)
-        {
-            if(!numset.contains(num-1))
-            {
-                int length=0;
-                while(numset.contains(length+num))
-                   length+=1;
-                longest=Math.max(length,longest);
+        int longest = 0;
+
+        for(int num:nums) {
+            if(!numset.contains(num-1)) {
+                int length =0;
+                while(numset.contains(num+length)) 
+                    length+=1;
+                longest = Math.max(length, longest);
             }
-                
-           
-            
         }
         return longest;
+        
     }
 }
