d2dd889a7e0c8dcd4b0b96aab69190224b0297cf
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-27 08:23:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (75.81%)

--- Leetcode problems/0206-reverse-linked-list/solution.java
+++ Leetcode problems/0206-reverse-linked-list/solution.java
@@ -10,19 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null)
-            return null;
-
-        ListNode curr = head;
-        ListNode prev =null;
+        ListNode curr= head;
+        ListNode prev = null;
 
         while(curr!=null) {
-            ListNode temp = curr.next;
+            ListNode temp=curr.next;
             curr.next = prev;
-            prev= curr;
-            curr= temp;
+            prev = curr;
+            curr = temp;
         }
-        return prev;
-        
+        return prev;  
     }
 }
