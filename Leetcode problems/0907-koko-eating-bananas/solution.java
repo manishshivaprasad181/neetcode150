fa65a6c87272648e2d492a74ab9188b8494a875c
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-27 08:18:19 +0000

    [LeetCode Sync] Runtime - 28 ms (20.45%), Memory - 45.6 MB (6.35%)

--- Leetcode problems/0907-koko-eating-bananas/solution.java
+++ Leetcode problems/0907-koko-eating-bananas/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left=1;
-        int right=1;
-        for(int pile:piles)
-            right=Math.max(right,pile);
-        while(left<right)
-        {
-            int middle=(right+left)/2;
-            int hourspent=0;
-            for(int pile:piles){
-                hourspent+=Math.ceil((double)pile/middle);
-            }
-            if(hourspent<=h)
-            right=middle;
+        int left =1;
+        int right = 1;
+        for(int pile:piles) 
+            right = Math.max(pile, right);
+        
+        while(left<right) {
+            int mid = left+(right-left)/2;
+            int hoursspent =0;
+            for(int pile:piles) 
+                hoursspent+= Math.ceil((double)pile/mid);
+            if(hoursspent<=h)
+                right= mid;
             else
-            left=middle+1;
+                left = mid+1;
         }
         return right;
-
     }
 }
