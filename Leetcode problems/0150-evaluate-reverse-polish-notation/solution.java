c9df4f8db246b54a39d594cfb97840212f9cf190
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-27 06:18:53 +0000

    [LeetCode Sync] Runtime - 6 ms (74.79%), Memory - 44.7 MB (39.64%)

--- Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
+++ Leetcode problems/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,23 +1,23 @@
 class Solution {
     public int evalRPN(String[] tokens) {
+        HashSet<String> hs =new HashSet<>();
         Stack<Integer> st = new Stack<>();
-        HashSet<String> hs= new HashSet<>();
         hs.add("+");
         hs.add("-");
         hs.add("*");
         hs.add("/");
 
         for(int i=0;i<tokens.length;i++) {
-            if(!hs.contains(tokens[i])) 
+            if(!hs.contains(tokens[i]))
                 st.push(Integer.parseInt(tokens[i]));
             else {
-                if(tokens[i].equals("+")) 
+                if(tokens[i].equals("+"))
                     st.push(st.pop()+st.pop());
                 else if(tokens[i].equals("-")) {
                     int x = st.pop();
                     int y = st.pop();
                     st.push(y-x);
-                } else if(tokens[i].equals("*")) 
+                } else if(tokens[i].equals("*"))
                     st.push(st.pop()*st.pop());
                 else {
                     int x = st.pop();
@@ -25,10 +25,7 @@ class Solution {
                     st.push(y/x);
                 }
             }
-
-            
         }
         return st.pop();
-        
     }
 }
