c0058a9c63d8690ee79c6a1c7cd4f38a4bf88e2e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-28 05:28:56 +0000

    [LeetCode Sync] Runtime - 2 ms (99.24%), Memory - 45.7 MB (54.53%)

--- Leetcode problems/0621-task-scheduler/solution.java
+++ Leetcode problems/0621-task-scheduler/solution.java
@@ -3,16 +3,15 @@ class Solution {
         int[] frequencies = new int[26];
         for(char task:tasks)
             frequencies[task-'A']++;
-
+        
         Arrays.sort(frequencies);
         int f_max = frequencies[25];
         int idle_time = (f_max-1)*n;
-        for(int i=frequencies.length-2;i>=0 && idle_time>0;i--) {
-            idle_time-= Math.min(f_max-1, frequencies[i]);
 
-        }
+        for(int i= frequencies.length-2;i>=0 && idle_time>0;i--)
+            idle_time-= Math.min(f_max-1, frequencies[i]);
+        
         idle_time= Math.max(0, idle_time);
         return idle_time+tasks.length;
-        
     }
 }
