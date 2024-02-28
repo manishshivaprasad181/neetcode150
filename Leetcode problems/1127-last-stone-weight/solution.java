6a02f6ad56ca356eecc4ba17a5af652a998e365e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-28 04:24:30 +0000

    [LeetCode Sync] Runtime - 1 ms (97.88%), Memory - 40.9 MB (67.21%)

--- Leetcode problems/1127-last-stone-weight/solution.java
+++ Leetcode problems/1127-last-stone-weight/solution.java
@@ -2,16 +2,17 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
 
-        for(int stone:stones) 
+        for(int stone:stones)
             heap.offer(stone);
         
         while(heap.size()>1) {
             int stone1 = heap.poll();
             int stone2 = heap.poll();
+
             if(stone1!=stone2) 
                 heap.offer(stone1-stone2);
+            
         }
         return heap.isEmpty()?0:heap.poll();
-        
     }
 }
