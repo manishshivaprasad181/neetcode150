20e600184d7a12fedeb4c31c61e99d6d7e8fed4f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-02-29 23:12:21 +0000

    [LeetCode Sync] Runtime - 16 ms (31.28%), Memory - 48.5 MB (53.33%)

--- Leetcode problems/0347-top-k-frequent-elements/solution.java
+++ Leetcode problems/0347-top-k-frequent-elements/solution.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(k==nums.length)
-            return nums;
-        
-        Map<Integer, Integer> hm = new HashMap<>();
-        for(int n:nums) {
-            hm.put(n, hm.getOrDefault(n, 0)+1);
-        }
+        HashMap<Integer, Integer> hm = new HashMap<>();
 
-        Queue<Integer> heap = new PriorityQueue<>((n1,n2)->hm.get(n1)-hm.get(n2));
+        for(int num:nums)
+            hm.put(num, hm.getOrDefault(num, 0)+1);
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1,n2)->hm.get(n1)-hm.get(n2));
 
         for(int n:hm.keySet()) {
             heap.add(n);
-            if(heap.size()>k) heap.poll();
+            if(heap.size()>k)
+                heap.poll();
         }
 
-        int[] top = new int[k];
-        for(int i=k-1;i>=0;i--) {
+        int[] top =new int[k];
+        for(int i = k-1;i>=0;i--)
             top[i] = heap.poll();
-        }
 
         return top;
-        
+
     }
 }
