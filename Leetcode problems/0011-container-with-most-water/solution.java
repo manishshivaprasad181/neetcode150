77fb3f84fdb87d96c5238d422d21698e3fb509c3
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 02:15:49 +0000

    [LeetCode Sync] Runtime - 5 ms (71.84%), Memory - 57.1 MB (84.63%)

--- Leetcode problems/0011-container-with-most-water/solution.java
+++ Leetcode problems/0011-container-with-most-water/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        int i=0;
-        int j = height.length-1;
-        int maxAmount =0;
-        while(i<j) {
-            maxAmount = Math.max(Math.min(height[i],height[j])*(j-i), maxAmount);
+        int left =0;
+        int right = height.length-1;
+        int maxAmount=0;
 
-            if(height[i]<height[j])
-                i++;
-            else 
-                j--;
+        while(left<right) {
+            maxAmount = Math.max(Math.min(height[left], height[right])*(right-left), maxAmount);
+
+            if(height[left]<height[right])
+                left++;
+            else
+                right--;    
         }
         return maxAmount;
-        
     }
 }
