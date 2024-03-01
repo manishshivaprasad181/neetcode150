565a6cdee64bc66388fb59c675abce209647396e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 05:06:53 +0000

    [LeetCode Sync] Runtime - 2 ms (93.99%), Memory - 46.3 MB (87.97%)

--- Leetcode problems/0338-counting-bits/solution.java
+++ Leetcode problems/0338-counting-bits/solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    private int popcount(int x) {
+    public int popCount(int x) {
         int count;
         for(count=0;x!=0;count++)
-            x&=x-1;
+            x = x&x-1;
         return count;
     }
+
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
+        for(int i=0;i<=n;i++)
+            ans[i] = popCount(i);
 
-        for(int i=0;i<=n;i++) {
-            ans[i]= popcount(i);
-        }
         return ans;
     }
 }
