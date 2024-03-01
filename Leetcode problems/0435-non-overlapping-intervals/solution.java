246086930825fc86d25b571a040d8bbadea8e030
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 23:06:44 +0000

    [LeetCode Sync] Runtime - 51 ms (39.65%), Memory - 91.9 MB (10.11%)

--- Leetcode problems/0435-non-overlapping-intervals/solution.java
+++ Leetcode problems/0435-non-overlapping-intervals/solution.java
@@ -1,20 +1,20 @@
 class Solution {
-  public int eraseOverlapIntervals(int[][] intervals) {
-      Arrays.sort(intervals, Comparator.comparingInt(a->a[1]));
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a->a[1]));
 
-      int ans=0;
-      int k = Integer.MIN_VALUE;
+        int ans=0;
+        int n =intervals.length;
+        int k = Integer.MIN_VALUE;
 
-      for(int i =0;i<intervals.length;i++) {
-          int x = intervals[i][0];
-          int y = intervals[i][1];
-
-          if(x>=k) 
-              k =y;
-            else 
+        for(int i=0;i<n;i++) {
+            int x =intervals[i][0];
+            int y = intervals[i][1];
+            
+            if(x>=k)
+                k=y;
+            else
                 ans++;
-          
-      }
-      return ans;
-  }
+        }
+        return ans;
+    }
 }
