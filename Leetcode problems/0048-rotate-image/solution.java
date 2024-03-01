5996dcdd4ce48fa8434f6b49e4a68903e895d1a0
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 07:06:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (82.69%)

--- Leetcode problems/0048-rotate-image/solution.java
+++ Leetcode problems/0048-rotate-image/solution.java
@@ -4,11 +4,11 @@ class Solution {
         reflect(matrix);
     }
     public void transpose(int[][] matrix) {
-        int n=matrix.length;
-        int temp;
+        int n= matrix.length;
+        int temp=0;
 
         for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
+            for(int j =i+1;j<n;j++) {
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -17,16 +17,12 @@ class Solution {
     }
     public void reflect(int[][] matrix) {
         int n = matrix.length;
-        int temp;
-
         for(int i=0;i<n;i++) {
-            for(int j =0;j<n/2;j++) {
-                temp = matrix[i][j];
+            for(int j=0;j<n/2;j++) {
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][n-j-1];
                 matrix[i][n-j-1] = temp;
             }
         }
     }
-
-   
 }
