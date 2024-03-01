f15c055b354d85e757a648f6a3821bcf7756f82a
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 04:03:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (24.72%)

--- Leetcode problems/0074-search-a-2d-matrix/solution.java
+++ Leetcode problems/0074-search-a-2d-matrix/solution.java
@@ -2,18 +2,18 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        if(m==0)
-            return false;
         int left =0;
         int right = m*n-1;
+
         while(left<=right) {
             int mid = left+(right-left)/2;
-            if(target==matrix[mid/n][mid%n]) 
+
+            if(matrix[mid/n][mid%n]==target)
                 return true;
-            else if(target>matrix[mid/n][mid%n])
-                left = mid+1;
-            else 
+            else if(matrix[mid/n][mid%n]>target)
                 right = mid-1;
+            else 
+                left=mid+1;
         }
         return false;
     }
