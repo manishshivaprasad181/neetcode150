26d8fff142b8510316c01db03c74496114b099bd
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 03:47:38 +0000

    [LeetCode Sync] Runtime - 64 ms (58.40%), Memory - 63.4 MB (7.28%)

--- Leetcode problems/0739-daily-temperatures/solution.java
+++ Leetcode problems/0739-daily-temperatures/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] res = new int[temperatures.length];
         Stack<Pair<Integer, Integer>> st = new Stack<>();
+        int[] res = new int[temperatures.length];
 
         for(int i=0;i<temperatures.length;i++) {
             while(!st.isEmpty() && temperatures[i]>st.peek().getKey()) {
                 Pair<Integer, Integer> pair = st.pop();
-                int index= pair.getValue();
+                int index = pair.getValue();
                 res[index] = i-index;
+
             }
-            st.push(new Pair(temperatures[i], i));
+            st.push(new Pair(temperatures[i],i));
         }
         return res;
-        
     }
 }
