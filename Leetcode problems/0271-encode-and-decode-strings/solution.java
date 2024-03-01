b95cb955a19fe28b1968a1de366418d80fd4fc54
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 00:16:10 +0000

    [LeetCode Sync] Runtime - 8 ms (52.46%), Memory - 45.6 MB (32.10%)

--- Leetcode problems/0271-encode-and-decode-strings/solution.java
+++ Leetcode problems/0271-encode-and-decode-strings/solution.java
@@ -3,18 +3,20 @@ public class Codec {
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder encodedString = new StringBuilder();
+
         for(String str:strs) {
             encodedString.append(str);
             encodedString.append("π");
+
         }
         return encodedString.toString();
-        
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        String[] decodedStrings = s.split("π",-1);
-        return new ArrayList(Arrays.asList(decodedStrings).subList(0,decodedStrings.length-1));
+        String[] decodedString = s.split("π",-1);
+        return new ArrayList(Arrays.asList(decodedString).subList(0, decodedString.length-1));
+        
     }
 }
 
