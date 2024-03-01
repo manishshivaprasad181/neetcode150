d208db15af368f844b2ec2f3ca7e72826c1a7e07
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-01 18:51:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (15.58%)

--- Leetcode problems/0050-powx-n/solution.java
+++ Leetcode problems/0050-powx-n/solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    private double binaryExp(double x, long n) {
+    public double binarExp(double x, long n) {
         if(n==0)
             return 1;
         if(n<0)
-            return 1.0/binaryExp(x, -1*n);
+            return 1/binarExp(x, -1*n);
         if(n%2==1)
-            return x*binaryExp(x*x, (n-1)/2);
-        else 
-            return binaryExp(x*x, n/2);
+            return x*binarExp(x*x, (n-1)/2);
+        else
+            return binarExp(x*x, n/2);
     }
     public double myPow(double x, int n) {
-        return binaryExp(x, (long)n);
+        return binarExp(x, (long)n);
     }
 }
