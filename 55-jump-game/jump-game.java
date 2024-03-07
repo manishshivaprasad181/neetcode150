e736a017a2d52ff832f83ffce8f3f7d9c438e13f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-06 21:02:00 -0500

    Time: 67 ms (18.99%) | Memory: 45.7 MB (45.68%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,26 @@
+enum Index{
+    GOOD,BAD,UNKNOWN
+}
+class Solution {
+    public boolean canJump(int[] nums) {
+        Index[] memo=new Index[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            memo[i]=Index.UNKNOWN;
+        }
+        memo[memo.length-1]=Index.GOOD;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            int furthestjump=Math.min(i+nums[i],nums.length-1);
+            for(int j=i+1;j<=furthestjump;j++)
+            {
+                if(memo[j]==Index.GOOD)
+                {
+                    memo[i]=Index.GOOD;
+                    break;
+                }
+            }
+        }
+        return memo[0]==Index.GOOD;
+    }
+}
