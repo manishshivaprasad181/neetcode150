72968092d98c7d78262eae4eba8a8edce71d154e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-07 02:50:25 -0500

    Time: 1 ms (99.96%) | Memory: 45.7 MB (45.68%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int lastpos=nums.length-1;
-        for(int i=nums.length-1;i>=0;i--)
+        int lastpos=nums.length-1;//4
+        for(int i=nums.length-2;i>=0;i--)
         {
             if(i+nums[i]>=lastpos){
                 lastpos=i;
