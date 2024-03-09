57375a2d28b1ebc04ac7c79d0fa0065ee863b5b1
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-09 20:12:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (36.62%)

--- Leetcode problems/0070-climbing-stairs/solution.java
+++ Leetcode problems/0070-climbing-stairs/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-      if(n==1)
-        return n;
-      if(n==2)
-        return n;
-      int first = 1;
-      int second =2;
-      for(int j=3;j<=n;j++) {
-        int third = first+second;
-        first=second;
-        second=third;
-      }
-      return second;
+        if(n==1)
+            return 1;
+
+        int[] dp = new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++) {
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
