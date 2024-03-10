3e15e296601d50d2a0699d91910c463caa511d8e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-10 01:06:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.6 MB (83.10%)

--- Leetcode problems/0198-house-robber/solution.java
+++ Leetcode problems/0198-house-robber/solution.java
@@ -2,14 +2,15 @@ class Solution {
     public int rob(int[] nums) {
         int N = nums.length;
 
-        int[] maxRobbedAmount = new int[N+1];
+        int[] maxRobbed = new int[N+1];
 
-        maxRobbedAmount[N]=0;
-        maxRobbedAmount[N-1]= nums[N-1];
+        maxRobbed[N]=0;
+        maxRobbed[N-1]= nums[N-1];
 
-        for(int i =N-2;i>=0;i--) {
-            maxRobbedAmount[i] = Math.max(maxRobbedAmount[i+1], maxRobbedAmount[i+2]+nums[i]);
+        for(int i=N-2;i>=0;i--) {
+            maxRobbed[i] = Math.max(maxRobbed[i+1], maxRobbed[i+2]+nums[i]);
         }
-        return maxRobbedAmount[0];
+        
+        return maxRobbed[0];
     }
 }
