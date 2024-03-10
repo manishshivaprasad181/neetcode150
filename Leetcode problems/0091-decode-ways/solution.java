457c617dbf065f6cade87c70c16998fc5d3f1152
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-10 10:30:18 +0000

    [LeetCode Sync] Runtime - 2 ms (21.17%), Memory - 41.7 MB (54.29%)

---
+++ Leetcode problems/0091-decode-ways/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    public int numDecodings(String s) {
+        return recursiveWithMemo(0,s);
+    }
+
+    private int recursiveWithMemo(int index, String str) {
+        if(memo.containsKey(index))
+            return memo.get(index);
+        
+     
+
+        
+        if(index== str.length())
+            return 1;
+        
+        if(str.charAt(index)=='0')
+            return 0;
+
+        if(index== str.length()-1)
+            return 1;
+      
+        
+        int ans = recursiveWithMemo(index+1, str);
+        if(Integer.parseInt(str.substring(index, index+2))<=26) 
+            ans+= recursiveWithMemo(index+2, str);
+        
+        memo.put(index, ans);
+        return ans;
+    }
+}
