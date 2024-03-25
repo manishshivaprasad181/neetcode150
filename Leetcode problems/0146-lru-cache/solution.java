f99d8b537af60fc2b2adee0caa9b68ef3e3dc78e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-25 17:02:01 +0000

    [LeetCode Sync] Runtime - 48 ms (50.46%), Memory - 124 MB (19.48%)

--- Leetcode problems/0146-lru-cache/solution.java
+++ Leetcode problems/0146-lru-cache/solution.java
@@ -1,73 +1,69 @@
-class ListNode {
-   int key;
-   int val;
-
-   ListNode prev;
-   ListNode next;
-
-   public ListNode(int key, int val) {
-       this.key = key;
-       this.val = val;
-   }
-}
-
+class ListNode{
+    int val;
+    int key;
+    ListNode prev;
+    ListNode next;
+    ListNode(int key, int val)
+    {
+        this.key=key;
+        this.val=val;
+    }
+} 
 class LRUCache {
     int capacity;
-    Map<Integer, ListNode> dic;
+    Map<Integer,ListNode> dic;
     ListNode head;
     ListNode tail;
-    
-
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        head = new ListNode(-1,-1);
-        tail = new ListNode(-1,-1);
-        dic = new HashMap<>();
-        head.next = tail;
-        tail.prev = head;
+        this.capacity=capacity;
+        head=new ListNode(-1,-1);
+        tail=new ListNode(-1,-1);
+        dic=new HashMap<>();
+        head.next=tail;
+        tail.prev=head;
+        
     }
     
     public int get(int key) {
         if(!dic.containsKey(key))
-            return -1;
-
-        ListNode node = dic.get(key);
+        return -1;
+        ListNode node=dic.get(key);
         remove(node);
         add(node);
         return node.val;
+        
     }
     
-    public void put(int key, int value) { 
-        if(dic.containsKey(key)) {
-            ListNode nodetobeDeleted = dic.get(key);
-            remove(nodetobeDeleted);
-        }
-
-
-        ListNode node = new ListNode(key, value);
-        add(node);
-        dic.put(key, node);
-
-        if(dic.size()>capacity) {
-            ListNode nodetobedeleted = head.next;
-             dic.remove(nodetobedeleted.key);
+    public void put(int key, int value) {
+        if(dic.containsKey(key))
+        {
+            ListNode nodetobedeleted=dic.get(key);
             remove(nodetobedeleted);
             
-           
+        }
+        ListNode node=new ListNode(key,value);
+        add(node);
+        dic.put(key,node);
+        if(dic.size()>capacity)
+        {
+            ListNode nodetobedeleted=head.next;
+            dic.remove(nodetobedeleted.key);
+            remove(nodetobedeleted);
         }
     }
-    public void add(ListNode node) {
-        ListNode previousEnd = tail.prev;
-        previousEnd.next = node;
-        node.prev = previousEnd;
-        node.next = tail;
-        tail.prev = node;
+    public void add(ListNode node)
+    {
+        ListNode previousEnd=tail.prev;
+        previousEnd.next=node;
+        node.prev=previousEnd;
+        node.next=tail;
+        tail.prev=node;
     }
-    public void remove(ListNode node) {
-        node.next.prev = node.prev;
-        node.prev.next = node.next;
+    public void remove(ListNode node)
+    {
+        node.next.prev=node.prev;
+        node.prev.next=node.next;
     }
-    
 }
 
 /**
