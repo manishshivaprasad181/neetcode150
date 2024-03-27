9ed64df94353620569a325d9e930f222ea6abb9e
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-27 07:07:18 +0000

    [LeetCode Sync] Runtime - 5 ms (91.27%), Memory - 44.2 MB (49.27%)

--- Leetcode problems/0300-longest-increasing-subsequence/solution.java
+++ Leetcode problems/0300-longest-increasing-subsequence/solution.java
@@ -1,20 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,1);
+        ArrayList<Integer> sub = new ArrayList<>();
+        sub.add(nums[0]);
 
         for(int i=1;i<nums.length;i++) {
-            for(int j=0;j<i;j++) {
-                if(nums[i]>nums[j])
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+            int num = nums[i];
+            if(num>sub.get(sub.size()-1))
+                sub.add(num);
+            else {
+                int j =0;
+                while(num>sub.get(j))
+                    j+=1;
+                sub.set(j, num);
             }
-
-        } 
-        int longest=0;
-        for(int c:dp) {
-            longest = Math.max(c, longest);
-
         }
-        return longest;
+        return sub.size();
     }
 }
