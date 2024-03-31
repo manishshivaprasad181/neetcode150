1306a42fd359c8a5c55a4f77bbecb6ad481712cd
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-03-31 22:46:31 +0000

    [LeetCode Sync] Runtime - 21 ms (88.56%), Memory - 41.7 MB (95.96%)

---
+++ Leetcode problems/0416-partition-equal-subset-sum/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if(nums.length==0)
+            return false;
+
+        int totalSum =0;
+        for(int num:nums) {
+            totalSum+= num;
+        }
+
+        if(totalSum%2!=0) return false;
+        int subSetSum = totalSum/2;
+        boolean dp[] = new boolean[subSetSum+1];
+        dp[0] = true;
+        for(int curr:nums) {
+            for(int j=subSetSum;j>=curr;j--) {
+                dp[j] |=dp[j-curr];
+            }
+        }        
+        return dp[subSetSum];
+    }
+}
