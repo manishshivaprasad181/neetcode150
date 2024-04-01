9d8ec339da6a8cdd13f61dfb5822e6078e0b80c5
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-04-01 03:45:45 +0000

    [LeetCode Sync] Runtime - 1 ms (60.95%), Memory - 42.6 MB (45.91%)

--- Leetcode problems/0078-subsets/solution.java
+++ Leetcode problems/0078-subsets/solution.java
@@ -1,18 +1,23 @@
 class Solution {
+    List<List<Integer>> output = new ArrayList<>();
+    int n,k;
+    public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
+        if(curr.size()==k) {
+            output.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = first;i<n;i++) {
+            curr.add(nums[i]);
+            backtrack(i+1, curr, nums);
+            curr.remove(curr.size()-1);
+        }
+    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> output=new ArrayList<>();
-        output.add(new ArrayList<Integer>());
-        for(int num:nums){
-            List<List<Integer>> newsubsets=new ArrayList();
-            for(List<Integer> curr:output){
-                newsubsets.add(new ArrayList<Integer>(curr){{add(num);}});
-            }
-            for(List<Integer> curr:newsubsets){
-                output.add(curr);
-            }
+        n = nums.length;
+        for(k=0;k<n+1;k++) {
+            backtrack(0, new ArrayList<>(), nums);
         }
         return output;
-        
-        
     }
 }
