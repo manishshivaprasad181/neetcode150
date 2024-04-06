b48a111c9d580b10eb44d4de1477878c52a44596
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-04-06 15:37:58 -0400

    Update reverse-nodes-in-k-group.java

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -40,3 +40,63 @@ class Solution {
        return head;
     }
 }
+
+
+
+// Another Approach O(1) Space 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverselinkedlist(ListNode head, int k)
+    {
+        ListNode new_head=null;
+        ListNode ptr=head;
+        while(k>0){
+            ListNode next_node=ptr.next;
+            ptr.next=new_head;
+            new_head=ptr;
+            ptr=next_node;
+            k--;
+        }
+        return new_head;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+       ListNode ptr = head;
+       ListNode ktail=null;
+       ListNode new_head=null;
+       while(ptr!=null)
+       {
+        int count=0;
+        ptr=head;
+        while(count<k && ptr!=null)
+        {
+            ptr=ptr.next;
+            count+=1;
+        }
+        if(count==k)
+        {
+            ListNode revhead=reverselinkedlist(head,k);
+            if(new_head==null)
+            new_head=revhead;
+            if(ktail!=null)
+            ktail.next=revhead;
+            ktail=head;
+            head=ptr;
+    
+            
+        }
+       }
+       if(ktail!=null)
+       ktail.next=head;
+       return new_head==null?head:new_head;
+
+    }
+}
