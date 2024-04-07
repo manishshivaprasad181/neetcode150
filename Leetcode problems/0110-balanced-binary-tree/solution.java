f6495e4b51187faf28a919ed70cb97a07849da9f
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2024-04-07 04:00:42 +0000

    [LeetCode Sync] Runtime - 1 ms (32.54%), Memory - 44.5 MB (31.87%)

--- Leetcode problems/0110-balanced-binary-tree/solution.java
+++ Leetcode problems/0110-balanced-binary-tree/solution.java
@@ -14,17 +14,17 @@
  * }
  */
 class Solution {
-    private int height(TreeNode node) {
-        if(node ==null)
+    private int height(TreeNode root)
+    {
+        if(root==null)
+        {
             return -1;
-        return 1+Math.max(height(node.left), height(node.right));
+        }
+        return Math.max(height(root.left),height(root.right))+1;
     }
     public boolean isBalanced(TreeNode root) {
-        if(root ==null)
-            return true;
-
-        return Math.abs(height(root.left)-height(root.right)) <2 && isBalanced(root.left) && isBalanced(root.right);
-
-        
+        if(root==null)
+        return true;
+        return Math.abs(height(root.left)-height(root.right))<2&& isBalanced(root.left)&&isBalanced(root.right);
     }
 }
